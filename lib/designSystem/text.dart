bf21c61f2aa9f018c4801529d66131d546a93cc6
cityshin <ssk7750@naver.com>
2024-01-31 14:35:32 +0900

    Feat. design system 추가

---
+++ lib/designSystem/text.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/cupertino.dart';
+
+class BircaText extends StatelessWidget{
+
+  final String text;
+  final int textSize;
+  final Color textColor;
+  final String fontFamily;
+
+  const BircaText({
+    required this.text,
+    required this.textSize,
+    required this.textColor,
+    required this.fontFamily,
+});
+
+  @override
+  Widget build(BuildContext context) {
+   return Text(text,
+   style: TextStyle(
+     color: textColor,
+     fontSize: textSize.toDouble(),
+     fontFamily: fontFamily
+   ),
+   );
+
+  }
+
+}
