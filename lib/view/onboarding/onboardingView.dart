169d784d306fde87053147f4456e4d7128de0db0
cityshin <ssk7750@naver.com>
2024-02-04 17:33:38 +0900

    Feat. 주최자 view

--- lib/view/onboarding/onboardingView.dart
+++ lib/view/onboarding/onboardingView.dart
@@ -1,3 +1,4 @@
+import 'package:birca/assets/designSystem/button.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import '../../assets/colors/mainColors.dart';
@@ -64,8 +65,7 @@ class _OnBoardingView extends State<OnBoardingView> {
                                     builder: (context) =>
                                         const OnboardingView2()));
                           },
-                          child:
-                              Image.asset('lib/assets/image/img_fan.png')),
+                          child: Image.asset('lib/assets/image/img_fan.png')),
                       const SizedBox(
                         width: 10,
                       ),
@@ -144,9 +144,15 @@ class OnboardingView2 extends StatelessWidget {
                         width: 10,
                       ),
                       GestureDetector(
-                          onTap: () {},
-                          child: Image.asset(
-                              'lib/assets/image/img_fan_host.png')),
+                          onTap: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) =>
+                                        OnboardingFanHostView()));
+                          },
+                          child:
+                              Image.asset('lib/assets/image/img_fan_host.png')),
                     ],
                   )
                 ],
@@ -336,6 +342,7 @@ class _OnboardingCafeOwnerView extends State<OnboardingCafeOwnerView> {
   }
 }
 
+//카페 사장 온보딩 완료
 class OnboardingCafeOwnerCompleteView extends StatelessWidget {
   const OnboardingCafeOwnerCompleteView({super.key});
 
@@ -389,5 +396,306 @@ class OnboardingCafeOwnerCompleteView extends StatelessWidget {
         ],
       ),
     ));
+  }
+}
+
+//주최자 온보딩
+class OnboardingFanHostView extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _OnboardingFanHostView();
+}
+
+class _OnboardingFanHostView extends State<OnboardingFanHostView> {
+  bool isDateChecked = false;
+  bool isCountChecked = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back_ios),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          margin: const EdgeInsets.only(left: 30, right: 30),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(
+                height: 24,
+              ),
+              RichText(
+                text: TextSpan(
+                    style: DefaultTextStyle.of(context).style,
+                    children: const [
+                      TextSpan(
+                        text: '어떤 생일 카페를\n',
+                        style: TextStyle(
+                            color: MainColors.grey10,
+                            fontSize: 24,
+                            decoration: TextDecoration.none,
+                            fontFamily: 'PretendardMedium'),
+                      ),
+                      TextSpan(
+                        text: '준비',
+                        style: TextStyle(
+                            color: MainColors.main03,
+                            fontSize: 30,
+                            decoration: TextDecoration.none,
+                            fontFamily: 'PretendardBold'),
+                      ),
+                      TextSpan(
+                        text: '하고 있나요?',
+                        style: TextStyle(
+                            color: MainColors.grey10,
+                            fontSize: 24,
+                            decoration: TextDecoration.none,
+                            fontFamily: 'PretendardMedium'),
+                      ),
+                    ]),
+              ),
+              const SizedBox(
+                height: 46,
+              ),
+              const Row(
+                children: [
+                  BircaText(
+                      text: '아티스트',
+                      textSize: 16,
+                      textColor: MainColors.grey10,
+                      fontFamily: 'PretendardMedium'),
+                  SizedBox(
+                    width: 2,
+                  ),
+                  Text(
+                    '*',
+                    style: TextStyle(color: Color(0xffFE2E2E)),
+                  )
+                ],
+              ),
+              Container(
+                width: 332,
+                child: TextField(
+                  decoration: InputDecoration(
+                      //비활성화
+                      enabledBorder: const UnderlineInputBorder(
+                          borderSide: BorderSide(color: Color(0xffD7D8DC))),
+                      hintText: '아티스트',
+                      suffixIcon: IconButton(
+                        onPressed: () {},
+                        icon: const Icon(Icons.search),
+                      )
+                      //활성화
+                      // focusedBorder: UnderlineInputBorder(
+                      //   borderSide: BorderSide(color: MainColors.main03)
+                      // )
+                      ),
+                ),
+              ),
+              SizedBox(
+                height: 48,
+              ),
+              BircaText(
+                  text: '생일카페 주최 일정',
+                  textSize: 16,
+                  textColor: MainColors.grey10,
+                  fontFamily: 'PretendardMedium'),
+              SizedBox(
+                height: 20,
+              ),
+              Row(children: [
+                Container(
+                  width: 238,
+                  height: 36,
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Color(0xffD7D8DC)),
+                    borderRadius: BorderRadius.circular(2), // 테두리 굴곡 설정
+                  ),
+                  child: Text(''),
+                ),
+                SizedBox(
+                  width: 11,
+                ),
+                BircaOutLinedButton(
+                  text: '날짜 선택',
+                  radiusColor: MainColors.main03,
+                  width: 80,
+                  height: 36,
+                  radius: 6,
+                  textColor: MainColors.main03,
+                  textSize: 14,
+                )
+              ]),
+              Row(
+                children: [
+                  Checkbox(
+                    value: isDateChecked,
+                    side: BorderSide(color: Color(0xffD7D8DC)),
+                    onChanged: (bool? value) {
+                      setState(() {
+                        isDateChecked = value ?? false;
+                      });
+                    },
+                  ),
+                  BircaText(
+                      text: '날짜 미정',
+                      textSize: 12,
+                      textColor: Color(0xff8f9093),
+                      fontFamily: 'PretendardRegular')
+                ],
+              ),
+              SizedBox(
+                height: 48,
+              ),
+              BircaText(
+                  text: '예상 방문자 수',
+                  textSize: 16,
+                  textColor: MainColors.grey10,
+                  fontFamily: 'PretendardMedium'),
+              SizedBox(
+                height: 24,
+              ),
+              Row(
+                children: [
+                  BircaText(
+                      text: '최소',
+                      textSize: 14,
+                      textColor: MainColors.grey10,
+                      fontFamily: 'PretendardLight'),
+                  Container(
+                    width: 60,
+                    child: TextField(
+                      textAlign: TextAlign.end,
+                      style: TextStyle(fontSize: 14),
+                      decoration: InputDecoration(
+                        isDense: true,
+
+                        contentPadding: EdgeInsets.zero,
+                        //비활성화
+                        enabledBorder: const UnderlineInputBorder(
+                            borderSide: BorderSide(color: Color(0xffD7D8DC))),
+                        hintText: '',
+
+                        //활성화
+                        // focusedBorder: UnderlineInputBorder(
+                        //   borderSide: BorderSide(color: MainColors.main03)
+                        // )
+                      ),
+                    ),
+                  ),
+                  BircaText(
+                      text: '명, ',
+                      textSize: 14,
+                      textColor: MainColors.grey10,
+                      fontFamily: 'PretendardLight'),
+                  BircaText(
+                      text: '최대',
+                      textSize: 14,
+                      textColor: MainColors.grey10,
+                      fontFamily: 'PretendardLight'),
+                  Container(
+                    width: 60,
+                    child: const TextField(
+                      textAlign: TextAlign.end,
+                      style: TextStyle(fontSize: 14),
+                      decoration: InputDecoration(
+                        isDense: true,
+                        contentPadding: EdgeInsets.zero,
+
+                        //비활성화
+                        enabledBorder: UnderlineInputBorder(
+                          borderSide: BorderSide
+                            (
+                            color: Color(0xffD7D8DC),
+
+                          ),
+                        ),
+
+                        hintText: '',
+
+                        //활성화
+                        // focusedBorder: UnderlineInputBorder(
+                        //   borderSide: BorderSide(color: MainColors.main03)
+                        // )
+                      ),
+                    ),
+                  ),
+                  BircaText(
+                      text: '명',
+                      textSize: 14,
+                      textColor: MainColors.grey10,
+                      fontFamily: 'PretendardLight'),
+                ],
+              ),
+
+              Row(
+                children: [
+                  Checkbox(
+                    value: isCountChecked,
+                    side: BorderSide(color: Color(0xffD7D8DC)),
+                    onChanged: (bool? value) {
+                      setState(() {
+                        isCountChecked = value ?? false;
+                      });
+                    },
+                  ),
+                  BircaText(
+                      text: '예상 방문자 수 미정',
+                      textSize: 12,
+                      textColor: Color(0xff8f9093),
+                      fontFamily: 'PretendardRegular')
+                ],
+              ),
+              SizedBox(height: 48,),
+              BircaText(
+                  text: '생일카페 트위터 계정',
+                  textSize: 16,
+                  textColor: MainColors.grey10,
+                  fontFamily: 'PretendardMedium'),
+
+
+                  SizedBox(height: 10,),
+                  Container(
+                    width: 200,
+                    child: TextField(
+                      style: TextStyle(
+                          fontSize: 16
+                      ),
+                      decoration: InputDecoration(
+                        isDense: true,
+                        contentPadding: EdgeInsets.zero,
+                        prefixIcon: Icon(Icons.alternate_email),
+                        prefixIconColor: Color(0xff8f9093),
+                        prefixIconConstraints: BoxConstraints(),
+                        //비활성화
+                        enabledBorder: const UnderlineInputBorder(
+                            borderSide: BorderSide(color: Color(0xffD7D8DC))),
+
+                        //활성화
+                        // focusedBorder: UnderlineInputBorder(
+                        //   borderSide: BorderSide(color: MainColors.main03)
+                        // )
+                      ),
+                    ),
+                  ),
+              SizedBox(height: 72,),
+
+              Container(
+                alignment: Alignment.center,
+                child:           BircaFilledButton(text: '버카 시작하기', color: Color(0xffbfc0c4), width: 300, height: 46)
+
+              ),
+
+              SizedBox(height: 100,)
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
