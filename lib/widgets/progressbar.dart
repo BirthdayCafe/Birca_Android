7733f18b54c6fea669b0836a508e48b7d41c16a8
Seunggu Kang <alex9366@naver.com>
2024-02-07 13:14:14 +0900

    feat: 온보딩 상태 진행 바 추가

---
+++ lib/widgets/progressbar.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+import '../assets/designsystem/palette.dart';
+
+ClipRRect progressBar(double value) => ClipRRect(
+    borderRadius: const BorderRadius.all(Radius.circular(10)),
+    child: LinearProgressIndicator(
+      value: value,
+      backgroundColor: Palette.gray02,
+      valueColor: const AlwaysStoppedAnimation<Color>(Palette.primary),
+    ));
