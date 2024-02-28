1c15b6cf96d27cfc2a565ab266b8e8622b37a4da
cityshin <ssk7750@naver.com>
2024-02-28 16:36:32 +0900

    feature. 주최자 페이지 추가

---
+++ lib/view/host/host_home.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class HostHome extends StatelessWidget{
+  const HostHome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    return const Scaffold(
+        body: Text('host_home')
+    );
+  }
+}
