188a02bcc21f8d9b3e029a16caa533d8b9c67cb4
cityshin <ssk7750@naver.com>
2024-03-01 18:12:23 +0900

    feature. 찜한 카페 없을 시

--- lib/view/visitor/visitor_favorite.dart
+++ lib/view/visitor/visitor_favorite.dart
@@ -1,13 +1,67 @@
+import 'package:birca/designSystem/text.dart';
 import 'package:flutter/material.dart';
 
-class VisitorFavorite extends StatelessWidget{
+import '../../designSystem/palette.dart';
+
+class VisitorFavorite extends StatefulWidget {
   const VisitorFavorite({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  State<StatefulWidget> createState() => _VisitorFavorite();
+}
 
-    return const Scaffold(
-        body: Text('visitor_heart')
-    );
+class _VisitorFavorite extends State<VisitorFavorite> {
+  bool isFavoriteExist = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          centerTitle: false, // 타이틀 왼쪽 정렬 설정
+          scrolledUnderElevation: 0,
+          title: RichText(
+            text: TextSpan(
+                style: DefaultTextStyle.of(context).style,
+                children: const [
+                  TextSpan(
+                    text: '찜',
+                    style: TextStyle(
+                        color: Palette.primary,
+                        fontSize: 20,
+                        decoration: TextDecoration.none,
+                        fontFamily: 'Pretendard',
+                        fontWeight: FontWeight.bold),
+                  ),
+                  TextSpan(
+                    text: '한 생일카페',
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 20,
+                        decoration: TextDecoration.none,
+                        fontFamily: 'PretendardMedium'),
+                  ),
+                ]),
+          ),
+        ),
+        body: isFavoriteExist
+            ? Container(
+                width: double.infinity, // 화면 전체 너비
+                height: double.infinity, // 화면 전체 높이
+                color: const Color(0xffF7F7FA),
+                child: const SingleChildScrollView())
+            : Container(
+                width: double.infinity, // 화면 전체 너비
+                height: double.infinity, // 화면 전체 높이
+                color: const Color(0xffF7F7FA),
+
+                child: Container(
+                  alignment: Alignment.center,
+                  child: const BircaText(
+                      text: '현재 찜한 카페가 없습니다',
+                      textSize: 16,
+                      textColor: Palette.gray06,
+                      fontFamily: 'Pretendard'),
+                ),
+              ));
   }
 }
