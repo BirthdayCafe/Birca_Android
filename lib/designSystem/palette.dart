21bc23a8f683acb049af4f32849fc2360cde343c
Seunggu Kang <alex9366@naver.com>
2024-03-06 19:20:47 +0900

    style: 색상 추가

--- lib/designSystem/palette.dart
+++ lib/designSystem/palette.dart
@@ -3,6 +3,7 @@ import 'dart:ui';
 class Palette {
   static const primary = Color(0xFFFF8282);
   static const background = Color(0xFFF7F7FA);
+  static const white = Color(0xFFFFFFFF);
   static const gray02 = Color(0xFFEFF0F5);
   static const gray03 = Color(0xFFD7D8DC);
   static const gray04 = Color(0xFFBFC0C4);
