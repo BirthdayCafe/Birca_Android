d9f1c60e002ea156329841a4be0fd4d43de80d01
cityshin <ssk7750@naver.com>
2024-03-12 13:19:02 +0900

    feature. host search

---
+++ lib/view/host/host_search.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HostSearch extends StatefulWidget {
+  const HostSearch({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return _HostSearch();
+  }
+}
+
+class _HostSearch extends State<HostSearch>{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+
+}
