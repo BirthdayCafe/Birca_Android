3f958ed95303b9d502e6dfd81724285961d741e0
cityshin <ssk7750@naver.com>
2024-03-25 13:44:47 +0900

    feature. 나의 생일카페 상세 화면 완료

--- lib/view/host/host_cafe_edit.dart
+++ lib/view/host/host_cafe_edit.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:table_calendar/table_calendar.dart';
 import '../../designSystem/palette.dart';
 import '../../designSystem/text.dart';
+import '../../widgets/button.dart';
 
 class HostCafeEdit extends StatefulWidget {
   const HostCafeEdit({super.key});
@@ -13,6 +15,23 @@ class HostCafeEdit extends StatefulWidget {
 
 class _HostCafeEdit extends State<HostCafeEdit> {
   bool isSwitched = false;
+  String hostDate = '';
+  bool isDateChecked = false;
+  bool isCountChecked = false;
+
+  RangeSelectionMode _rangeSelectionMode = RangeSelectionMode.toggledOn;
+  DateTime? _selectedDay;
+  DateTime _focusedDay = DateTime.now();
+  DateTime? _rangeStart;
+  DateTime? _rangeEnd;
+
+  List<String> cafeDetailImage = [
+    'lib/assets/image/img_cafe_test.png',
+    'lib/assets/image/img_cafe_test.png',
+    'lib/assets/image/img_cafe_test.png',
+    'lib/assets/image/img_cafe_test.png',
+    'lib/assets/image/img_cafe_test.png'
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -167,10 +186,10 @@ class _HostCafeEdit extends State<HostCafeEdit> {
             ),
             Container(
               padding: const EdgeInsets.only(left: 18, right: 18, top: 40),
-              child: const Column(
+              child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text(
+                  const Text(
                     '카페 이름',
                     style: TextStyle(
                         color: Palette.gray10,
@@ -178,16 +197,16 @@ class _HostCafeEdit extends State<HostCafeEdit> {
                         fontWeight: FontWeight.w700,
                         fontSize: 16),
                   ),
-                  TextField(
+                  const TextField(
                     decoration: InputDecoration(
                       hintText: '아티스트 및 그룹명',
                       border: UnderlineInputBorder(), // 밑줄 추가
                     ),
                   ),
-                  SizedBox(
+                  const SizedBox(
                     height: 26,
                   ),
-                  Text(
+                  const Text(
                     '주최자 정보',
                     style: TextStyle(
                         color: Palette.gray10,
@@ -195,16 +214,16 @@ class _HostCafeEdit extends State<HostCafeEdit> {
                         fontWeight: FontWeight.w700,
                         fontSize: 16),
                   ),
-                  TextField(
+                  const TextField(
                     decoration: InputDecoration(
                       hintText: '트위터 계정',
                       border: UnderlineInputBorder(), // 밑줄 추가
                     ),
                   ),
-                  SizedBox(
+                  const SizedBox(
                     height: 26,
                   ),
-                  Text(
+                  const Text(
                     '생일 카페 주최 일정',
                     style: TextStyle(
                         color: Palette.gray10,
@@ -212,6 +231,148 @@ class _HostCafeEdit extends State<HostCafeEdit> {
                         fontWeight: FontWeight.w700,
                         fontSize: 16),
                   ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  Row(children: [
+                    Container(
+                      width: 238,
+                      height: 36,
+                      decoration: BoxDecoration(
+                        border: Border.all(color: const Color(0xffD7D8DC)),
+                        borderRadius: BorderRadius.circular(2), // 테두리 굴곡 설정
+                      ),
+                      alignment: Alignment.center,
+                      child: Text(
+                        hostDate,
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 11,
+                    ),
+                    BircaOutLinedButton(
+                      text: '날짜 선택',
+                      radiusColor: Palette.primary,
+                      width: 80,
+                      height: 36,
+                      radius: 6,
+                      textColor: Palette.primary,
+                      textSize: 14,
+                      onPressed: () {
+                        _showBottomDialogCalendar(context);
+                      },
+                      backgroundColor: Colors.white,
+                    )
+                  ]),
+                  const SizedBox(
+                    height: 26,
+                  ),
+                  const Text(
+                    '카페 위치',
+                    style: TextStyle(
+                        color: Palette.gray10,
+                        fontFamily: 'Pretendard',
+                        fontWeight: FontWeight.w700,
+                        fontSize: 16),
+                  ),
+                  const TextField(
+                    decoration: InputDecoration(
+                      hintText: '서울 특별시~~',
+                      border: UnderlineInputBorder(), // 밑줄 추가
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 26,
+                  ),
+                  const Text(
+                    '특전 구성',
+                    style: TextStyle(
+                        color: Palette.gray10,
+                        fontFamily: 'Pretendard',
+                        fontWeight: FontWeight.w700,
+                        fontSize: 16),
+                  ),
+                  const Text(
+                    '* 특전 종류와 구성품을 작성해주세요.',
+                    style: TextStyle(
+                        color: Palette.gray08,
+                        fontFamily: 'Pretendard',
+                        fontWeight: FontWeight.w400,
+                        fontSize: 10),
+                  ),
+                  const SizedBox(
+                    height: 26,
+                  ),
+                  const Text(
+                    '생일 카페 메뉴',
+                    style: TextStyle(
+                        color: Palette.gray10,
+                        fontFamily: 'Pretendard',
+                        fontWeight: FontWeight.w700,
+                        fontSize: 16),
+                  ),
+                  const Text(
+                    '* 메뉴 구성을 작성해주세요.',
+                    style: TextStyle(
+                        color: Palette.gray08,
+                        fontFamily: 'Pretendard',
+                        fontWeight: FontWeight.w400,
+                        fontSize: 10),
+                  ),
+                  const SizedBox(
+                    height: 26,
+                  ),
+                  const Text(
+                    '럭키 드로우',
+                    style: TextStyle(
+                        color: Palette.gray10,
+                        fontFamily: 'Pretendard',
+                        fontWeight: FontWeight.w700,
+                        fontSize: 16),
+                  ),
+                  const Text(
+                    '* 럭키드로우 구성을 작성해주세요.',
+                    style: TextStyle(
+                        color: Palette.gray08,
+                        fontFamily: 'Pretendard',
+                        fontWeight: FontWeight.w400,
+                        fontSize: 10),
+                  ),
+                  const SizedBox(
+                    height: 26,
+                  ),
+                  const Text(
+                    '사진',
+                    style: TextStyle(
+                        color: Palette.gray10,
+                        fontFamily: 'Pretendard',
+                        fontWeight: FontWeight.w700,
+                        fontSize: 16),
+                  ),
+                  const Text(
+                    '* 생일 카페 관련 사진은 최대 10장까지 업로드 가능합니다.',
+                    style: TextStyle(
+                        color: Palette.gray08,
+                        fontFamily: 'Pretendard',
+                        fontWeight: FontWeight.w400,
+                        fontSize: 10),
+                  ),
+                  SizedBox(
+                      height: 90,
+                      child: ListView.builder(
+                          shrinkWrap: true, // shrinkWrap을 true로 설정
+
+                          scrollDirection: Axis.horizontal,
+                          itemCount: cafeDetailImage.length,
+                          itemBuilder: (context, index) {
+                            return Container(
+                              padding: const EdgeInsets.only(right: 8),
+                              child: Image.asset(cafeDetailImage[index]),
+                            );
+                          })),
+                  const SizedBox(
+                    height: 26,
+                  )
                 ],
               ),
             )
@@ -219,5 +380,89 @@ class _HostCafeEdit extends State<HostCafeEdit> {
         ),
       ),
     );
+  }
+
+  void _showBottomDialogCalendar(BuildContext context) async {
+    var hostDate1 = await showModalBottomSheet(
+        context: context,
+        builder: (context) {
+          return StatefulBuilder(
+              builder: (BuildContext context, StateSetter setState) {
+            //
+            return Column(
+              children: [
+                Container(
+                  padding: const EdgeInsets.only(
+                      left: 25, right: 25, top: 10, bottom: 13),
+                  child: TableCalendar(
+                    //오늘 날짜
+                    focusedDay: _focusedDay,
+                    firstDay: DateTime.now(),
+                    lastDay: DateTime.utc(DateTime.now().year + 1),
+
+                    headerStyle: const HeaderStyle(
+                        formatButtonVisible: false, titleCentered: true),
+
+                    rangeStartDay: _rangeStart,
+                    rangeEndDay: _rangeEnd,
+                    rangeSelectionMode: _rangeSelectionMode,
+
+                    selectedDayPredicate: (day) {
+                      return isSameDay(_selectedDay, day);
+                    },
+
+                    onDaySelected: (selectedDay, focusedDay) {
+                      if (!isSameDay(_selectedDay, selectedDay)) {
+                        setState(() {
+                          _selectedDay = selectedDay;
+                          _focusedDay =
+                              focusedDay; // update `_focusedDay` here as well
+                          _rangeStart = null; // Important to clean those
+                          _rangeEnd = null;
+                          _rangeSelectionMode = RangeSelectionMode.toggledOff;
+                        });
+                      }
+                    },
+
+                    //달력 날짜 범위 선택
+                    onRangeSelected: (start, end, focusedDay) {
+                      setState(() {
+                        _selectedDay = null;
+                        _focusedDay = focusedDay;
+                        _rangeStart = start;
+                        _rangeEnd = end;
+                        _rangeSelectionMode = RangeSelectionMode.toggledOn;
+                        // print('start : $_rangeStart / end : $_rangeEnd ');
+                      });
+                    },
+                  ),
+                ),
+                BircaFilledButton(
+                  text: '적용하기',
+                  color: Palette.primary,
+                  width: 300,
+                  height: 46,
+                  onPressed: () {
+                    //날짜를 하나만 선택 했을 시
+                    _rangeEnd ??= _rangeStart;
+                    setState(() {
+                      hostDate =
+                          '${_rangeStart?.year}.${_rangeStart?.month}.${_rangeStart?.day}~${_rangeEnd?.year}.${_rangeEnd?.month}.${_rangeEnd?.day}';
+                      // print(hostDate);
+                    });
+
+                    Navigator.of(context).pop(hostDate);
+                  },
+                ),
+              ],
+            );
+          });
+        });
+
+    if (hostDate1 != null) {
+      setState(() {
+        hostDate = hostDate1;
+      });
+    }
   }
 }
