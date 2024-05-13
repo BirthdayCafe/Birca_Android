c28fc53a2a666dc95ed7e268fa2183ea25472070
cityshin <ssk7750@naver.com>
2024-05-13 16:00:38 +0900

    feature. 사장님 홈 화면 model

---
+++ lib/model/owner_home_model.dart
@@ -0,0 +1,39 @@
+class OwnerHomeModel {
+  final int birthdayCafeId;
+  final String hostName;
+  final String startDate;
+  final String endDate;
+  final Artist artist;
+
+  OwnerHomeModel({
+    required this.hostName,
+    required this.birthdayCafeId,
+    required this.startDate,
+    required this.endDate,
+    required this.artist,
+  });
+
+  factory OwnerHomeModel.fromJson(Map<String, dynamic> json) {
+    return OwnerHomeModel(
+      birthdayCafeId: json['birthdayCafeId'],
+      hostName: json['hostName'],
+      startDate: json['startDate'],
+      endDate: json['endDate'],
+      artist: Artist.fromJson(json['artist']),
+    );
+  }
+}
+
+class Artist {
+  final String groupName;
+  final String name;
+
+  Artist({required this.groupName, required this.name});
+
+  factory Artist.fromJson(Map<String, dynamic> json) {
+    return Artist(
+      groupName: json['groupName'],
+      name: json['name'],
+    );
+  }
+}
