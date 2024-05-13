90073acb7cbeac51a9a0ef0a610a47db2b7aa0e0
cityshin <ssk7750@naver.com>
2024-05-13 16:00:32 +0900

    feature. 사장님 홈 화면 view model

---
+++ lib/viewModel/owner_home_view_model.dart
@@ -0,0 +1,91 @@
+import 'dart:developer';
+
+import 'package:dio/dio.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+import '../model/owner_home_model.dart';
+
+class OwnerHomeViewModel extends ChangeNotifier{
+  Dio dio = Dio();
+
+  List<OwnerHomeModel>? _ownerHomeModelList;
+
+  List<OwnerHomeModel>? get ownerHomeModelList =>
+      _ownerHomeModelList;
+
+  OwnerHomeViewModel(){
+    _ownerHomeModelList = [];
+  }
+
+  //주최자 카페 홈 가져오기
+  Future<void> getOwnerHome(String progressState) async {
+    // const storage = FlutterSecureStorage();
+    var baseUrl = dotenv.env['BASE_URL'];
+    var token = '';
+    //
+    // var kakaoLoginInfo = await storage.read(key: 'kakaoLoginInfo');
+
+    // 토큰 가져오기
+    // if (kakaoLoginInfo != null) {
+    //   Map<String, dynamic> loginData = json.decode(kakaoLoginInfo);
+    //   token = loginData['accessToken'].toString();
+    // }
+    token ='eyJhbGciOiJIUzI1NiJ9.eyJpZCI6MywiaWF0IjoxNzE1MjE2MjA2LCJleHAiOjE3MTc4MDgyMDZ9.v1HNk52QlUDjKoO-Um5AHS_eBj5GscsLZ2upP455kvQ';
+
+    // LogInterceptor 추가
+    dio.interceptors.add(LogInterceptor(
+      requestBody: true,
+      responseBody: true,
+    ));
+
+    try {
+      // API 엔드포인트 및 업로드
+      Response response = await dio.get('${baseUrl}api/v1/owners/birthday-cafes',
+
+          queryParameters: {
+            'progressState': progressState,
+          },
+          options: Options(headers: {'Authorization': 'Bearer $token'}));
+
+      // 서버 응답 출력
+      log('Response: ${response.data}');
+
+      List<dynamic> jsonData = response.data;
+      List<OwnerHomeModel> cafeHomeModels =
+      jsonData.map((e) => OwnerHomeModel.fromJson(e)).toList();
+
+      // _hostCafeHomeModelList 추가
+      _ownerHomeModelList?.addAll(cafeHomeModels);
+
+      notifyListeners();
+    } catch (e) {
+      if (e is DioException) {
+        // Dio exception handling
+        if (e.response != null) {
+          // Server responded with an error
+          if (e.response!.statusCode == 400) {
+            // Handle HTTP 400 Bad Request error
+            log('Bad Request - Server returned 400 status code');
+            throw Exception('Failed to getOwnerHome');
+
+            // Additional error handling logic here if needed
+          } else {
+            // Handle other HTTP status codes
+            log('Server error - Status code: ${e.response!.statusCode}');
+            throw Exception('Failed to getOwnerHome.');
+            // Additional error handling logic here if needed
+          }
+        } else {
+          // No response from the server (network error, timeout, etc.)
+          log('Dio error: ${e.message}');
+          throw Exception('Failed to getOwnerHome.');
+        }
+      } else {
+        // Handle other exceptions if necessary
+        log('Error: $e');
+        throw Exception('Failed to getOwnerHome.');
+      }
+    }
+  }
+}
