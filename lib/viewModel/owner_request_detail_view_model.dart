3b0ef18898df944f9ec345ade0073ff77fc505a4
cityshin <ssk7750@naver.com>
2024-05-14 13:33:59 +0900

    feature. 카페 신청 수락,거절 api 추가

--- lib/viewModel/owner_request_detail_view_model.dart
+++ lib/viewModel/owner_request_detail_view_model.dart
@@ -12,7 +12,7 @@ class OwnerRequestDetailViewModel extends ChangeNotifier {
   OwnerRequestDetailModel? get ownerRequestDetailModel =>
       _ownerRequestDetailModel;
 
-  //주최자 카페 홈 가져오기
+  //사장님 대관 요청 상세 가져오기
   Future<void> getRequestDetailHome(int birthdayCafeId) async {
     // const storage = FlutterSecureStorage();
     var baseUrl = dotenv.env['BASE_URL'];
@@ -26,7 +26,7 @@ class OwnerRequestDetailViewModel extends ChangeNotifier {
     //   token = loginData['accessToken'].toString();
     // }
     token =
-        'eyJhbGciOiJIUzI1NiJ9.eyJpZCI6MywiaWF0IjoxNzE1MjE2MjA2LCJleHAiOjE3MTc4MDgyMDZ9.v1HNk52QlUDjKoO-Um5AHS_eBj5GscsLZ2upP455kvQ';
+        'eyJhbGciOiJIUzI1NiJ9.eyJpZCI6MywiaWF0IjoxNzE1NjQ4MTk1LCJleHAiOjE3MjA4MzIxOTV9.yFY9Y18aPo4t1XA5ANsnfvqqnJsmq7kalNfj7FcGEi4';
 
     // LogInterceptor 추가
     dio.interceptors.add(LogInterceptor(
@@ -37,7 +37,7 @@ class OwnerRequestDetailViewModel extends ChangeNotifier {
     try {
       // API 엔드포인트 및 업로드
       Response response = await dio.get(
-          '${baseUrl}api/v1/owners/birthday-cafes$birthdayCafeId',
+          '${baseUrl}api/v1/owners/birthday-cafes/$birthdayCafeId',
           options: Options(headers: {'Authorization': 'Bearer $token'}));
 
       // 서버 응답 출력
@@ -76,4 +76,129 @@ class OwnerRequestDetailViewModel extends ChangeNotifier {
       }
     }
   }
+
+  //생일 카페 신청 수락
+  Future<void> postApprove(int birthdayCafeId) async {
+    // const storage = FlutterSecureStorage();
+    var baseUrl = dotenv.env['BASE_URL'];
+    var token = '';
+    //
+    // var kakaoLoginInfo = await storage.read(key: 'kakaoLoginInfo');
+
+    // 토큰 가져오기
+    // if (kakaoLoginInfo != null) {
+    //   Map<String, dynamic> loginData = json.decode(kakaoLoginInfo);
+    //   token = loginData['accessToken'].toString();
+    // }
+    token =
+    'eyJhbGciOiJIUzI1NiJ9.eyJpZCI6MywiaWF0IjoxNzE1NjQ4MTk1LCJleHAiOjE3MjA4MzIxOTV9.yFY9Y18aPo4t1XA5ANsnfvqqnJsmq7kalNfj7FcGEi4';
+
+    // LogInterceptor 추가
+    dio.interceptors.add(LogInterceptor(
+      requestBody: true,
+      responseBody: true,
+    ));
+
+    try {
+      // API 엔드포인트 및 업로드
+      Response response = await dio.post(
+          '${baseUrl}api/v1/owners/birthday-cafes/$birthdayCafeId/approve',
+          options: Options(headers: {'Authorization': 'Bearer $token'}));
+
+      // 서버 응답 출력
+      log('Response: ${response.data}');
+
+      notifyListeners();
+    } catch (e) {
+      if (e is DioException) {
+        // Dio exception handling
+        if (e.response != null) {
+          // Server responded with an error
+          if (e.response!.statusCode == 400) {
+            // Handle HTTP 400 Bad Request error
+            log('Bad Request - Server returned 400 status code');
+            throw Exception('Failed to postApprove');
+
+            // Additional error handling logic here if needed
+          } else {
+            // Handle other HTTP status codes
+            log('Server error - Status code: ${e.response!.statusCode}');
+            throw Exception('Failed to postApprove.');
+            // Additional error handling logic here if needed
+          }
+        } else {
+          // No response from the server (network error, timeout, etc.)
+          log('Dio error: ${e.message}');
+          throw Exception('Failed to postApprove.');
+        }
+      } else {
+        // Handle other exceptions if necessary
+        log('Error: $e');
+        throw Exception('Failed to postApprove.');
+      }
+    }
+  }
+
+  //생일 카페 신청 수락
+  Future<void> postCancel(int birthdayCafeId) async {
+    // const storage = FlutterSecureStorage();
+    var baseUrl = dotenv.env['BASE_URL'];
+    var token = '';
+    //
+    // var kakaoLoginInfo = await storage.read(key: 'kakaoLoginInfo');
+
+    // 토큰 가져오기
+    // if (kakaoLoginInfo != null) {
+    //   Map<String, dynamic> loginData = json.decode(kakaoLoginInfo);
+    //   token = loginData['accessToken'].toString();
+    // }
+    token =
+    'eyJhbGciOiJIUzI1NiJ9.eyJpZCI6MywiaWF0IjoxNzE1NjQ4MTk1LCJleHAiOjE3MjA4MzIxOTV9.yFY9Y18aPo4t1XA5ANsnfvqqnJsmq7kalNfj7FcGEi4';
+
+    // LogInterceptor 추가
+    dio.interceptors.add(LogInterceptor(
+      requestBody: true,
+      responseBody: true,
+    ));
+
+    try {
+      // API 엔드포인트 및 업로드
+      Response response = await dio.post(
+          '${baseUrl}api/v1/owners/birthday-cafes/$birthdayCafeId/cancel',
+          options: Options(headers: {'Authorization': 'Bearer $token'}));
+
+      // 서버 응답 출력
+      log('Response: ${response.data}');
+
+      notifyListeners();
+    } catch (e) {
+      if (e is DioException) {
+        // Dio exception handling
+        if (e.response != null) {
+          // Server responded with an error
+          if (e.response!.statusCode == 400) {
+            // Handle HTTP 400 Bad Request error
+            log('Bad Request - Server returned 400 status code');
+            throw Exception('Failed to postApprove');
+
+            // Additional error handling logic here if needed
+          } else {
+            // Handle other HTTP status codes
+            log('Server error - Status code: ${e.response!.statusCode}');
+            throw Exception('Failed to postApprove.');
+            // Additional error handling logic here if needed
+          }
+        } else {
+          // No response from the server (network error, timeout, etc.)
+          log('Dio error: ${e.message}');
+          throw Exception('Failed to postApprove.');
+        }
+      } else {
+        // Handle other exceptions if necessary
+        log('Error: $e');
+        throw Exception('Failed to postApprove.');
+      }
+    }
+  }
+
 }
