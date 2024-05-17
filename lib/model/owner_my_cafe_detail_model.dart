6bde172c7e124f5cf29c6ca9634170d671403ae9
cityshin <ssk7750@naver.com>
2024-05-17 16:08:58 +0900

    feature. 사장님 나의 카페 model

---
+++ lib/model/owner_my_cafe_detail_model.dart
@@ -0,0 +1,63 @@
+class OwnerMyCafeDetailModel {
+  String cafeName;
+  String cafeAddress;
+  String twitterAccount;
+  String businessHours;
+  List<MenuModel> cafeMenus;
+  List<OptionModel> cafeOptions;
+  List<String> cafeImages;
+
+  OwnerMyCafeDetailModel({
+    required this.cafeName,
+    required this.cafeAddress,
+    required this.twitterAccount,
+    required this.businessHours,
+    required this.cafeMenus,
+    required this.cafeOptions,
+    required this.cafeImages,
+  });
+
+  factory OwnerMyCafeDetailModel.fromJson(Map<String, dynamic> json) {
+    List<dynamic> menuList = json['cafeMenus'] ?? [];
+    List<dynamic> optionList = json['cafeOptions'] ?? [];
+    List<dynamic> imageList = json['cafeImages'] ?? [];
+
+    return OwnerMyCafeDetailModel(
+        cafeName: json['cafeName'],
+        cafeAddress: json['cafeAddress'],
+        twitterAccount: json['twitterAccount'],
+        businessHours: json['businessHours'],
+        cafeMenus: menuList.map((menu) => MenuModel.fromJson(menu)).toList(),
+        cafeOptions:
+            optionList.map((option) => OptionModel.fromJson(option)).toList(),
+        cafeImages: imageList.map((image) => image.toString()).toList());
+  }
+}
+
+class MenuModel {
+  final String name;
+  final int price;
+
+  MenuModel({required this.name, required this.price});
+
+  factory MenuModel.fromJson(Map<String, dynamic> json) {
+    return MenuModel(
+      name: json['name'],
+      price: json['price'],
+    );
+  }
+}
+
+class OptionModel {
+  final String name;
+  final int price;
+
+  OptionModel({required this.name, required this.price});
+
+  factory OptionModel.fromJson(Map<String, dynamic> json) {
+    return OptionModel(
+      name: json['name'],
+      price: json['price'],
+    );
+  }
+}
