306ac3165051663d5f953d95c8875b0a2bcdc7b6
cityshin <ssk7750@naver.com>
2024-05-17 15:12:33 +0900

    feature. model name 수정

--- lib/model/owner_request_detail_model.dart
+++ lib/model/owner_request_detail_model.dart
@@ -3,29 +3,29 @@ class OwnerRequestDetailModel {
   String startDate;
   String endDate;
   Artist artist;
-  int minimumVisitants;
-  int maximumVisitants;
-  String twiiterAccount;
+  int minimumVisitant;
+  int maximumVisitant;
+  String twitterAccount;
   String hostPhoneNumber;
 
   OwnerRequestDetailModel(
       {required this.birthdayCafeId,
-      required this.minimumVisitants,
-      required this.maximumVisitants,
+      required this.minimumVisitant,
+      required this.maximumVisitant,
       required this.startDate,
       required this.endDate,
-      required this.twiiterAccount,
+      required this.twitterAccount,
       required this.artist,
       required this.hostPhoneNumber});
 
   factory OwnerRequestDetailModel.fromJson(Map<String, dynamic> json) {
     return OwnerRequestDetailModel(
         birthdayCafeId: json['birthdayCafeId'],
-        minimumVisitants: json['minimumVisitants'],
-        maximumVisitants: json['maximumVisitants'],
+        minimumVisitant: json['minimumVisitant'],
+        maximumVisitant: json['maximumVisitant'],
         startDate: json['startDate'],
         endDate: json['endDate'],
-        twiiterAccount: json['twiiterAccount'],
+        twitterAccount: json['twitterAccount'],
         artist: Artist.fromJson(json['artist']),
         hostPhoneNumber: json['hostPhoneNumber']);
   }
