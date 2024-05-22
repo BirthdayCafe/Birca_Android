530e207ecd4cf979d72d7837e46c75402089944a
cityshin <ssk7750@naver.com>
2024-05-22 14:47:06 +0900

    feature. 주최자 카페 목록 api 연결

--- lib/view/host/host_home.dart
+++ lib/view/host/host_home.dart
@@ -1,4 +1,5 @@
 import 'package:birca/view/host/host_search.dart';
+import 'package:birca/viewModel/host_home_view_model.dart';
 import 'package:birca/viewModel/host_my_cafe_view_model.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -17,109 +18,109 @@ class HostHome extends StatefulWidget {
 }
 
 class _HostHome extends State<HostHome> {
+  @override
+  void initState() {
+    super.initState();
+    Provider.of<HostHomeViewModel>(context, listen: false).getHostHome(1, 3);
+  }
 
-  // @override
-  // void initState() {
-  //   super.initState();
-  //   Provider.of<HostCafeHomeViewModel>(context,listen: false).getHostCafeHome();
-  //
-  // }
   var isSwitched = false;
-  var cafeList = ['aaa', 'bbb', 'cc', 'd', 'e', 'f', 'g' 'h', 'i', 'j'];
+
+  // var cafeList = ['aaa', 'bbb', 'cc', 'd', 'e', 'f', 'g' 'h', 'i', 'j'];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        scrolledUnderElevation: 0,
-        title: SvgPicture.asset('lib/assets/image/birca.svg'),
-        actions: [
-          IconButton(
-              onPressed: () {
-                Navigator.of(context).push(MaterialPageRoute(
-                    builder: (context) => const HostSearch()));
-              },
-              icon: SvgPicture.asset('lib/assets/image/img_search.svg'))
-        ],
-      ),
-      body: SingleChildScrollView(
-
-        child: Consumer<HostMyCafeViewModel>(
-          builder: (context, viewModel, widget) {
-            return  Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Container(
-                  margin: const EdgeInsets.only(left: 16, right: 16),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Row(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          RichText(
-                            text: TextSpan(
-                                style: DefaultTextStyle.of(context).style,
-                                children: const [
-                                  TextSpan(
-                                    text: '대관 가능한',
-                                    style: TextStyle(
-                                      color: Palette.primary,
+        appBar: AppBar(
+          scrolledUnderElevation: 0,
+          title: SvgPicture.asset('lib/assets/image/birca.svg'),
+          actions: [
+            IconButton(
+                onPressed: () {
+                  Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => const HostSearch()));
+                },
+                icon: SvgPicture.asset('lib/assets/image/img_search.svg'))
+          ],
+        ),
+        body: SingleChildScrollView(child: Consumer<HostMyCafeViewModel>(
+            builder: (context, viewModel, widget) {
+          return Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                margin: const EdgeInsets.only(left: 16, right: 16),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        RichText(
+                          text: TextSpan(
+                              style: DefaultTextStyle.of(context).style,
+                              children: const [
+                                TextSpan(
+                                  text: '대관 가능한',
+                                  style: TextStyle(
+                                    color: Palette.primary,
+                                    fontSize: 20,
+                                    decoration: TextDecoration.none,
+                                    fontFamily: 'Pretendard',
+                                  ),
+                                ),
+                                TextSpan(
+                                  text: ' 카페',
+                                  style: TextStyle(
+                                      color: Palette.gray10,
                                       fontSize: 20,
                                       decoration: TextDecoration.none,
                                       fontFamily: 'Pretendard',
-                                    ),
-                                  ),
-                                  TextSpan(
-                                    text: ' 카페',
-                                    style: TextStyle(
-                                        color: Palette.gray10,
-                                        fontSize: 20,
-                                        decoration: TextDecoration.none,
-                                        fontFamily: 'Pretendard',
-                                        fontWeight: FontWeight.w500),
-                                  ),
-                                ]),
-                          ),
-                          SizedBox(
-                              width: 30,
-                              child: IconButton(
-                                onPressed: () {},
-                                icon: SvgPicture.asset(
-                                  'lib/assets/image/img_filter.svg',
+                                      fontWeight: FontWeight.w500),
                                 ),
-                              )),
-                        ],
-                      ),
-                      Row(
-                        children: [
-                          const BircaText(
-                              text: '찜',
-                              textSize: 15,
-                              textColor: Palette.gray06,
-                              fontFamily: 'Pretendard'),
-                          SizedBox(
-                              height: 10,
-                              child: Transform.scale(
-                                  scale: 0.7,
-                                  child: CupertinoSwitch(
-                                    value: isSwitched,
-                                    activeColor: Palette.primary,
-                                    onChanged: (value) {
-                                      setState(() {
-                                        isSwitched = value;
-                                      });
-                                    },
-                                  )))
-                        ],
-                      ),
-                    ],
-                  ),
+                              ]),
+                        ),
+                        SizedBox(
+                            width: 30,
+                            child: IconButton(
+                              onPressed: () {},
+                              icon: SvgPicture.asset(
+                                'lib/assets/image/img_filter.svg',
+                              ),
+                            )),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        const BircaText(
+                            text: '찜',
+                            textSize: 15,
+                            textColor: Palette.gray06,
+                            fontFamily: 'Pretendard'),
+                        SizedBox(
+                            height: 10,
+                            child: Transform.scale(
+                                scale: 0.7,
+                                child: CupertinoSwitch(
+                                  value: isSwitched,
+                                  activeColor: Palette.primary,
+                                  onChanged: (value) {
+                                    setState(() {
+                                      isSwitched = value;
+                                    });
+                                  },
+                                )))
+                      ],
+                    ),
+                  ],
                 ),
-                Row(
+              ),
+              Consumer<HostHomeViewModel>(
+                  builder: (builder, viewModel, widget) {
+                return Row(
                   children: [
                     const SizedBox(
                       width: 16,
@@ -132,7 +133,8 @@ class _HostHome extends State<HostHome> {
                           border: Border.all(
                             color: Palette.gray06,
                           ),
-                          borderRadius: BorderRadius.circular(20)), // 원하는 패딩 값 설정
+                          borderRadius: BorderRadius.circular(20)),
+                      // 원하는 패딩 값 설정
 
                       child: const Row(
                         children: [
@@ -163,7 +165,8 @@ class _HostHome extends State<HostHome> {
                           border: Border.all(
                             color: Palette.gray06,
                           ),
-                          borderRadius: BorderRadius.circular(20)), // 원하는 패딩 값 설정
+                          borderRadius: BorderRadius.circular(20)),
+                      // 원하는 패딩 값 설정
 
                       child: const Row(
                         children: [
@@ -184,113 +187,117 @@ class _HostHome extends State<HostHome> {
                       ),
                     )
                   ],
-                ),
-                const SizedBox(
-                  height: 30,
-                ),
-                ListView.builder(
-                    shrinkWrap: true,
-                    physics: const NeverScrollableScrollPhysics(),
-                    scrollDirection: Axis.vertical,
-                    itemCount: viewModel.hostMyCafeModelList?.length,
-                    itemBuilder: (BuildContext context, int index) {
-                      return Container(
-                        margin:
-                        const EdgeInsets.only(left: 16, right: 16, bottom: 16),
-                        // padding: const EdgeInsets.all(8),
-                        decoration: BoxDecoration(
-                          color: Colors.white, // Container의 배경색
-                          borderRadius: BorderRadius.circular(3),
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.grey.withOpacity(0.1), // 그림자 색상
-                              spreadRadius: 1, // 그림자 확산 정도
-                              blurRadius: 1, // 그림자의 흐림 정도
-                              // offset: Offset(0, 3), // 그림자의 위치 조절 (가로, 세로)
-                            ),
-                          ],
+                );
+              }),
+              const SizedBox(
+                height: 30,
+              ),
+    Consumer<HostHomeViewModel>(
+    builder: (builder, viewModel, widget) {
+      return ListView.builder(
+          shrinkWrap: true,
+          physics: const NeverScrollableScrollPhysics(),
+          scrollDirection: Axis.vertical,
+          itemCount: viewModel.hostCafeHomeModelList!.length,
+          itemBuilder: (BuildContext context, int index) {
+            return Container(
+              margin: const EdgeInsets.only(
+                  left: 16, right: 16, bottom: 16),
+              decoration: BoxDecoration(
+                color: Colors.white, // Container의 배경색
+                borderRadius: BorderRadius.circular(3),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.1),
+                    // 그림자 색상
+                    spreadRadius: 1,
+                    // 그림자 확산 정도
+                    blurRadius: 1, // 그림자의 흐림 정도
+                    // offset: Offset(0, 3), // 그림자의 위치 조절 (가로, 세로)
+                  ),
+                ],
+              ),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  //이미지
+                  SizedBox(
+                    height: 140,
+                    width: 210,
+                    child: Image.network(
+                      viewModel
+                          .hostCafeHomeModelList![index].cafeImageUrl,
+                      fit: BoxFit.fill,
+                    ),
+                  ),
+
+                  //카페 정보
+
+                  Container(
+                    padding: const EdgeInsets.only(left: 10, top: 40),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text(
+                          // '${viewModel.hostCafeHomeModelList?[index].birthdayCafeName}',
+                          '카페 이름',
+                          style: TextStyle(
+                              fontSize: 14,
+                              color: Palette.gray10,
+                              fontWeight: FontWeight.w600),
                         ),
-                        child: Row(
-                          crossAxisAlignment: CrossAxisAlignment.start,
+                        const SizedBox(
+                          height: 21,
+                        ),
+                         Text(
+                          '${viewModel.hostCafeHomeModelList?[index].twitterAccount}',
+                          style: const TextStyle(
+                              fontSize: 12,
+                              color: Palette.gray08,
+                              fontWeight: FontWeight.w400),
+                        ),
+                        const SizedBox(
+                          height: 5,
+                        ),
+                         Row(
                           children: [
-                            //이미지
-                            SizedBox(
-                              height: 140,
-                              width: 210,
-                              child: Image.network(
-                                // 'lib/assets/image/img_cafe_test.png',
-                                viewModel.hostMyCafeModelList![index].mainImageUrl,
-                                fit: BoxFit.fill,
-                              ),
+                            const Icon(
+                              Icons.location_on_outlined,
+                              color: Palette.gray08,
+                              size: 12,
                             ),
-
-                            //카페 정보
-
-                            Container(
-                              padding: const EdgeInsets.only(left: 10, top: 40),
-                              child:  Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text(
-                                    '${viewModel.hostMyCafeModelList?[index].birthdayCafeName}',
-                                    style: const TextStyle(
-                                        fontSize: 14,
-                                        color: Palette.gray10,
-                                        fontWeight: FontWeight.w600),
-                                  ),
-                                  const SizedBox(
-                                    height: 21,
-                                  ),
-                                  const Text(
-                                    '@twitter',
-                                    style: TextStyle(
-                                        fontSize: 12,
-                                        color: Palette.gray08,
-                                        fontWeight: FontWeight.w400),
-                                  ),
-                                  const SizedBox(
-                                    height: 5,
-                                  ),
-                                  const Row(
-                                    children: [
-                                      Icon(
-                                        Icons.location_on_outlined,
-                                        color: Palette.gray08,
-                                        size: 12,
-                                      ),
-                                      Text(
-                                        '서울특별시~~',
-                                        style: TextStyle(
-                                            fontSize: 12,
-                                            color: Palette.gray08,
-                                            fontWeight: FontWeight.w400),
-                                      ),
-                                    ],
-                                  ),
-                                ],
-                              ),
+                            Text(
+                              '${viewModel.hostCafeHomeModelList?[index].address.substring(0,10)}',
+                              style: const TextStyle(
+                                  fontSize: 12,
+                                  color: Palette.gray08,
+                                  fontWeight: FontWeight.w400),
                             ),
-
-                            Expanded(child: Container()),
-
-                            //heart
-                            Container(
-                              padding: const EdgeInsets.only(top: 8, right: 8),
-                              child: const Icon(
-                                Icons.favorite,
-                                color: Color(0xffF3F3F3),
-                              ),
-                            )
                           ],
                         ),
-                      );
-                    })
-              ],
-            );
-          }
-        )
+                      ],
+                    ),
+                  ),
 
-      ),
+                  Expanded(child: Container()),
+
+                  //heart
+                  Container(
+                    padding: const EdgeInsets.only(top: 8, right: 8),
+                    child: const Icon(
+                      Icons.favorite,
+                      color: Color(0xffF3F3F3),
+                    ),
+                  )
+                ],
+              ),
+            );
+          });
+    }),
+
+            ],
+          );
+        })),
         floatingActionButton: SizedBox(
           width: 86,
           height: 38,
@@ -318,7 +325,6 @@ class _HostHome extends State<HostHome> {
             ), // 버튼 모양
           ),
         ),
-        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat
-    );
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat);
   }
 }
