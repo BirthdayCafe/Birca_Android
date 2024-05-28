58df1922fe6dbe3e75b13b81662f3e9ccd599ef5
cityshin <ssk7750@naver.com>
2024-05-28 14:08:21 +0900

    feature. liked 변수 추가

--- lib/model/host_cafe_home_model.dart
+++ lib/model/host_cafe_home_model.dart
@@ -1,12 +1,13 @@
 class HostCafeHomeModel {
-   int cafeId;
-   String cafeImageUrl;
-   String twitterAccount;
-   String address;
+  int cafeId;
+  bool liked;
+  String cafeImageUrl;
+  String twitterAccount;
+  String address;
 
-
-   HostCafeHomeModel({
+  HostCafeHomeModel({
     required this.cafeId,
+    required this.liked,
     required this.cafeImageUrl,
     required this.twitterAccount,
     required this.address,
@@ -14,10 +15,11 @@ class HostCafeHomeModel {
 
   factory HostCafeHomeModel.fromJson(Map<String, dynamic> json) {
     return HostCafeHomeModel(
-        cafeId: json['cafeId'],
-        cafeImageUrl: json['cafeImageUrl'],
-        twitterAccount: json['twitterAccount'],
-        address: json['address'],);
+      cafeId: json['cafeId'],
+      liked: json['liked'],
+      cafeImageUrl: json['cafeImageUrl'],
+      twitterAccount: json['twitterAccount'],
+      address: json['address'],
+    );
   }
 }
-
