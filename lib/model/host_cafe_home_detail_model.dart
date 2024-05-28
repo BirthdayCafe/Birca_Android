62d2b1d42621457290987901ff6cc0545225721b
cityshin <ssk7750@naver.com>
2024-05-28 14:53:47 +0900

    feature. 주최자 카페 홈 상세 model

---
+++ lib/model/host_cafe_home_detail_model.dart
@@ -0,0 +1,40 @@
+class HostCafeHomeDetailModel {
+  String name;
+  String twitterAccount;
+  String address;
+  String businessHours;
+  RentalSchedules rentalSchedules;
+
+
+  HostCafeHomeDetailModel({
+    required this.name,
+    required this.businessHours,
+    required this.twitterAccount,
+    required this.address,
+    required this.rentalSchedules
+  });
+
+  factory HostCafeHomeDetailModel.fromJson(Map<String, dynamic> json) {
+    return HostCafeHomeDetailModel(
+      name: json['name'],
+      businessHours: json['businessHours'],
+      address: json['address'],
+      twitterAccount: json['twitterAccount'],
+      rentalSchedules: RentalSchedules.fromJson(json['rentalSchedules']),
+    );
+  }
+}
+
+class RentalSchedules {
+   String startDate;
+   String endDate;
+
+  RentalSchedules({required this.startDate, required this.endDate});
+
+  factory RentalSchedules.fromJson(Map<String, dynamic> json) {
+    return RentalSchedules(
+      startDate: json['startDate'],
+      endDate: json['endDate'],
+    );
+  }
+}
