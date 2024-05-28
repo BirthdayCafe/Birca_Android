940b0836aaa211969f13ed973a55a250d434e344
cityshin <ssk7750@naver.com>
2024-05-28 13:40:31 +0900

    feature. 사진 전송 api

--- lib/viewModel/owner_my_cafe_view_model.dart
+++ lib/viewModel/owner_my_cafe_view_model.dart
@@ -1,7 +1,9 @@
 import 'dart:developer';
+import 'dart:io';
 import 'package:dio/dio.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:image_picker/image_picker.dart';
 import '../model/owner_my_cafe_detail_model.dart';
 
 class OwnerMyCafeViewModel extends ChangeNotifier {
@@ -75,13 +77,19 @@ class OwnerMyCafeViewModel extends ChangeNotifier {
   }
 
   //사장님 나의 카페 편집
-  Future<void> patchMyCafe(String cafeName, String cafeAddress,String twitter, String hours,List<Map<String, Object>> cafeMenu,List<Map<String, Object>> cafeOptions) async {
+  Future<void> patchMyCafe(
+      String cafeName,
+      String cafeAddress,
+      String twitter,
+      String hours,
+      List<Map<String, Object>> cafeMenu,
+      List<Map<String, Object>> cafeOptions) async {
     // const storage = FlutterSecureStorage();
     var baseUrl = dotenv.env['BASE_URL'];
     var token = '';
 
     token =
-    "eyJhbGciOiJIUzI1NiJ9.eyJpZCI6MywiaWF0IjoxNzE1NjQ4MTk1LCJleHAiOjE3MjA4MzIxOTV9.yFY9Y18aPo4t1XA5ANsnfvqqnJsmq7kalNfj7FcGEi4";
+        "eyJhbGciOiJIUzI1NiJ9.eyJpZCI6MywiaWF0IjoxNzE1NjQ4MTk1LCJleHAiOjE3MjA4MzIxOTV9.yFY9Y18aPo4t1XA5ANsnfvqqnJsmq7kalNfj7FcGEi4";
     // var kakaoLoginInfo = await storage.read(key: 'kakaoLoginInfo');
     //
     // // 토큰 가져오기
@@ -100,19 +108,17 @@ class OwnerMyCafeViewModel extends ChangeNotifier {
       // API 엔드포인트 및 업로드
       Response response = await dio.patch('${baseUrl}api/v1/cafes',
           data: {
-        'cafeName' : cafeName,
-            'cafeAddress' :cafeAddress,
-            'twitterAccount' : twitter,
+            'cafeName': cafeName,
+            'cafeAddress': cafeAddress,
+            'twitterAccount': twitter,
             'businessHours': hours,
             'cafeMenus': cafeMenu,
             'cafeOptions': cafeOptions,
-
           },
           options: Options(headers: {'Authorization': 'Bearer $token'}));
 
       // 서버 응답 출력
       log('patchMyCafe Response: ${response.data}');
-
 
       notifyListeners();
     } catch (e) {
@@ -141,6 +147,142 @@ class OwnerMyCafeViewModel extends ChangeNotifier {
         // Handle other exceptions if necessary
         log('Error: $e');
         throw Exception('Failed to patchMyCafe.');
+      }
+    }
+  }
+
+  //사장님 휴무일 편집
+  Future<void> postDayOff(int cafeId, Map<String, dynamic> data) async {
+    // const storage = FlutterSecureStorage();
+    var baseUrl = dotenv.env['BASE_URL'];
+    var token = '';
+
+    token =
+        "eyJhbGciOiJIUzI1NiJ9.eyJpZCI6MywiaWF0IjoxNzE1NjQ4MTk1LCJleHAiOjE3MjA4MzIxOTV9.yFY9Y18aPo4t1XA5ANsnfvqqnJsmq7kalNfj7FcGEi4";
+    // var kakaoLoginInfo = await storage.read(key: 'kakaoLoginInfo');
+    //
+    // // 토큰 가져오기
+    // if (kakaoLoginInfo != null) {
+    //   Map<String, dynamic> loginData = json.decode(kakaoLoginInfo);
+    //   token = loginData['accessToken'].toString();
+    // }
+
+    // LogInterceptor 추가
+    dio.interceptors.add(LogInterceptor(
+      requestBody: true,
+      responseBody: true,
+    ));
+
+    try {
+      // API 엔드포인트 및 업로드
+      Response response = await dio.patch(
+          '${baseUrl}api/v1/cafes/$cafeId/day-offs',
+          data: {data},
+          options: Options(headers: {'Authorization': 'Bearer $token'}));
+
+      // 서버 응답 출력
+      log('postDayOff Response: ${response.data}');
+
+      notifyListeners();
+    } catch (e) {
+      if (e is DioException) {
+        // Dio exception handling
+        if (e.response != null) {
+          // Server responded with an error
+          if (e.response!.statusCode == 400) {
+            // Handle HTTP 400 Bad Request error
+            log('Bad Request - Server returned 400 status code');
+            throw Exception('Failed to postDayOff');
+
+            // Additional error handling logic here if needed
+          } else {
+            // Handle other HTTP status codes
+            log('Server error - Status code: ${e.response!.statusCode}');
+            throw Exception('Failed to postDayOff.');
+            // Additional error handling logic here if needed
+          }
+        } else {
+          // No response from the server (network error, timeout, etc.)
+          log('Dio error: ${e.message}');
+          throw Exception('Failed to postDayOff.');
+        }
+      } else {
+        // Handle other exceptions if necessary
+        log('Error: $e');
+        throw Exception('Failed to postDayOff.');
+      }
+    }
+  }
+
+  //사장님 카페 사진 편집
+  Future<void> postImage(int cafeId, List<PickedFile> pickedFiles) async {
+    // const storage = FlutterSecureStorage();
+    var baseUrl = dotenv.env['BASE_URL'];
+    var token = '';
+
+    token =
+        "eyJhbGciOiJIUzI1NiJ9.eyJpZCI6MywiaWF0IjoxNzE1NjQ4MTk1LCJleHAiOjE3MjA4MzIxOTV9.yFY9Y18aPo4t1XA5ANsnfvqqnJsmq7kalNfj7FcGEi4";
+    // var kakaoLoginInfo = await storage.read(key: 'kakaoLoginInfo');
+    //
+    // // 토큰 가져오기
+    // if (kakaoLoginInfo != null) {
+    //   Map<String, dynamic> loginData = json.decode(kakaoLoginInfo);
+    //   token = loginData['accessToken'].toString();
+    // }
+
+    // PickedFile 리스트를 File 리스트로 변환
+    List<File> files =
+        pickedFiles.map((pickedFile) => File(pickedFile.path)).toList();
+
+    // 업로드할 파일을 FormData로 변환
+    FormData formData = FormData();
+    for (int i = 0; i < files.length; i++) {
+      formData.files
+          .add(MapEntry('files', await MultipartFile.fromFile(files[i].path)));
+    }
+    // LogInterceptor 추가
+    dio.interceptors.add(LogInterceptor(
+      requestBody: true,
+      responseBody: true,
+    ));
+
+    try {
+      // API 엔드포인트 및 업로드
+      Response response = await dio.patch(
+          '${baseUrl}api/v1/cafes/$cafeId/images',
+          data: {formData},
+          options: Options(headers: {'Authorization': 'Bearer $token'}));
+
+      // 서버 응답 출력
+      log('postImage Response: ${response.data}');
+
+      notifyListeners();
+    } catch (e) {
+      if (e is DioException) {
+        // Dio exception handling
+        if (e.response != null) {
+          // Server responded with an error
+          if (e.response!.statusCode == 400) {
+            // Handle HTTP 400 Bad Request error
+            log('Bad Request - Server returned 400 status code');
+            throw Exception('Failed to postImage');
+
+            // Additional error handling logic here if needed
+          } else {
+            // Handle other HTTP status codes
+            log('Server error - Status code: ${e.response!.statusCode}');
+            throw Exception('Failed to postImage.');
+            // Additional error handling logic here if needed
+          }
+        } else {
+          // No response from the server (network error, timeout, etc.)
+          log('Dio error: ${e.message}');
+          throw Exception('Failed to postImage.');
+        }
+      } else {
+        // Handle other exceptions if necessary
+        log('Error: $e');
+        throw Exception('Failed to postImage.');
       }
     }
   }
