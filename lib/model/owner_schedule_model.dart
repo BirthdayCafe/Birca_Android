66e02d35c13015780003b2b13f93c03fa8bbbb1a
cityshin <ssk7750@naver.com>
2024-05-31 17:22:37 +0900

    feature. 사장님 스케줄 model

---
+++ lib/model/owner_schedule_model.dart
@@ -0,0 +1,68 @@
+class OwnerScheduleModel {
+  int birthdayCafeId;
+  String startDate;
+  String endDate;
+
+  OwnerScheduleModel({
+    required this.birthdayCafeId,
+    required this.startDate,
+    required this.endDate,
+  });
+
+  factory OwnerScheduleModel.fromJson(Map<String, dynamic> json) {
+    return OwnerScheduleModel(
+      birthdayCafeId: json['birthdayCafeId'],
+      startDate: json['startDate'],
+      endDate: json['endDate'],
+    );
+  }
+}
+
+class OwnerScheduleAddModel {
+  Artist artist;
+  String startDate;
+  String endDate;
+  int minimumVisitants;
+  int maximumVisitants;
+  String twitterAccount;
+  String hostPhoneNumber;
+
+  OwnerScheduleAddModel({
+    required this.artist,
+    required this.startDate,
+    required this.endDate,
+    required this.minimumVisitants,
+    required this.maximumVisitants,
+    required this.twitterAccount,
+    required this.hostPhoneNumber,
+  });
+
+  factory OwnerScheduleAddModel.fromJson(Map<String, dynamic> json) {
+    return OwnerScheduleAddModel(
+      artist: Artist.fromJson(json['artist']),
+      startDate: json['startDate'],
+      endDate: json['endDate'],
+      minimumVisitants: json['minimumVisitants'],
+      maximumVisitants: json['maximumVisitants'],
+      twitterAccount: json['twitterAccount'],
+      hostPhoneNumber: json['hostPhoneNumber'],
+    );
+  }
+}
+
+class Artist {
+  final String? groupName;
+  final String name;
+
+  Artist({
+    this.groupName,
+    required this.name,
+  });
+
+  factory Artist.fromJson(Map<String, dynamic> json) {
+    return Artist(
+      groupName: json['groupName'],
+      name: json['name'],
+    );
+  }
+}
