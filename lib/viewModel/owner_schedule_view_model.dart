ed31c335ab32bd936fb40cf3d98ebd10b6a9a55d
cityshin <ssk7750@naver.com>
2024-06-03 15:13:30 +0900

    feature. 사장님 스케줄 조회 api 추가

--- lib/viewModel/owner_schedule_view_model.dart
+++ lib/viewModel/owner_schedule_view_model.dart
@@ -5,13 +5,12 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-
 class OwnerScheduleViewModel extends ChangeNotifier {
   Dio dio = Dio();
 
-  // OwnerScheduleAddModel? _ownerScheduleAddModel;
-  //
-  // OwnerScheduleAddModel? get ownerScheduleAddModel => _ownerScheduleAddModel;
+  List<OwnerScheduleModel>? _ownerScheduleModel;
+
+  List<OwnerScheduleModel>? get ownerScheduleModel => _ownerScheduleModel;
 
   //사장님 스케줄 추가
   Future<void> postSchedule(OwnerScheduleAddModel ownerScheduleAddModel) async {
@@ -37,13 +36,13 @@ class OwnerScheduleViewModel extends ChangeNotifier {
 
     try {
       // API 엔드포인트 및 업로드
-      Response response = await dio.post('${baseUrl}api/v1/owners/birthday-cafes/schedules',
+      Response response = await dio.post(
+          '${baseUrl}api/v1/owners/birthday-cafes/schedules',
           data: ownerScheduleAddModel.toJson(),
           options: Options(headers: {'Authorization': 'Bearer $token'}));
 
       // 서버 응답 출력
       log('postSchedule Response: ${response.data}');
-
 
       notifyListeners();
     } catch (e) {
@@ -76,9 +75,72 @@ class OwnerScheduleViewModel extends ChangeNotifier {
     }
   }
 
+  //사장님 스케줄 가져오기
+  Future<void> getSchedule(int year, int month) async {
+    // const storage = FlutterSecureStorage();
+    var baseUrl = dotenv.env['BASE_URL'];
+    var token = '';
 
+    token =
+        "eyJhbGciOiJIUzI1NiJ9.eyJpZCI6MywiaWF0IjoxNzE1NjQ4MTk1LCJleHAiOjE3MjA4MzIxOTV9.yFY9Y18aPo4t1XA5ANsnfvqqnJsmq7kalNfj7FcGEi4";
+    // var kakaoLoginInfo = await storage.read(key: 'kakaoLoginInfo');
+    //
+    // // 토큰 가져오기
+    // if (kakaoLoginInfo != null) {
+    //   Map<String, dynamic> loginData = json.decode(kakaoLoginInfo);
+    //   token = loginData['accessToken'].toString();
+    // }
 
+    // LogInterceptor 추가
+    dio.interceptors.add(LogInterceptor(
+      requestBody: true,
+      responseBody: true,
+    ));
 
+    try {
+      // API 엔드포인트 및 업로드
+      Response response = await dio.get(
+          '${baseUrl}api/v1/owners/birthday-cafes/schedules',
+          queryParameters: {'year': year, 'month': month},
+          options: Options(headers: {'Authorization': 'Bearer $token'}));
 
+      _ownerScheduleModel =[];
+      List<dynamic> jsonData = response.data;
+      List<OwnerScheduleModel> scheduleModels =
+      jsonData.map((e) => OwnerScheduleModel.fromJson(e)).toList();
+      _ownerScheduleModel?.addAll(scheduleModels);
 
+      // 서버 응답 출력
+      log('getSchedule Response: ${response.data}');
+
+      notifyListeners();
+    } catch (e) {
+      if (e is DioException) {
+        // Dio exception handling
+        if (e.response != null) {
+          // Server responded with an error
+          if (e.response!.statusCode == 400) {
+            // Handle HTTP 400 Bad Request error
+            log('Bad Request - Server returned 400 status code');
+            throw Exception('Failed to getSchedule');
+
+            // Additional error handling logic here if needed
+          } else {
+            // Handle other HTTP status codes
+            log('Server error - Status code: ${e.response!.statusCode}');
+            throw Exception('Failed to getSchedule.');
+            // Additional error handling logic here if needed
+          }
+        } else {
+          // No response from the server (network error, timeout, etc.)
+          log('Dio error: ${e.message}');
+          throw Exception('Failed to getSchedule.');
+        }
+      } else {
+        // Handle other exceptions if necessary
+        log('Error: $e');
+        throw Exception('Failed to getSchedule.');
+      }
+    }
+  }
 }
