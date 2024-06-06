60feaf5252262d9b1032e4c74051ae17727f1f9a
cityshin <ssk7750@naver.com>
2024-06-06 18:31:46 +0900

    feature. 대관 신청 취소 api 추가

--- lib/viewModel/host_my_cafe_view_model.dart
+++ lib/viewModel/host_my_cafe_view_model.dart
@@ -30,7 +30,7 @@ class HostMyCafeViewModel extends ChangeNotifier{
     return progressStateMapping[progressState] ?? '알 수 없음';
   }
 
-  //주최자 카페 홈 가져오기
+  //주최자 나의 생일 카페 목록 가져오기
   Future<void> getHostMyCafe() async {
     // const storage = FlutterSecureStorage();
     var baseUrl = dotenv.env['BASE_URL'];
@@ -100,4 +100,64 @@ class HostMyCafeViewModel extends ChangeNotifier{
     }
   }
 
+  //주최자 나의 생일 카페 대관 신청 취소 가져오기
+  Future<void> postCancel(int cafeId) async {
+    // const storage = FlutterSecureStorage();
+    var baseUrl = dotenv.env['BASE_URL'];
+    var token = '';
+    //
+    // var kakaoLoginInfo = await storage.read(key: 'kakaoLoginInfo');
+
+    // 토큰 가져오기
+    // if (kakaoLoginInfo != null) {
+    //   Map<String, dynamic> loginData = json.decode(kakaoLoginInfo);
+    //   token = loginData['accessToken'].toString();
+    // }
+    token ='eyJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwiaWF0IjoxNzE2MjkwNDAxLCJleHAiOjE3MjE0NzQ0MDF9.8BaCgTdEVbBMf1tApT9le3_LtBU69QW6SESucv6jiM0';
+
+    // LogInterceptor 추가
+    dio.interceptors.add(LogInterceptor(
+      requestBody: true,
+      responseBody: true,
+    ));
+
+    try {
+      // API 엔드포인트 및 업로드
+      Response response = await dio.post('${baseUrl}api/v1/birthday-cafes/$cafeId/cancel',
+
+          options: Options(headers: {'Authorization': 'Bearer $token'}));
+
+      // 서버 응답 출력
+      log('Response: ${response.data}');
+
+      notifyListeners();
+    } catch (e) {
+      if (e is DioException) {
+        // Dio exception handling
+        if (e.response != null) {
+          // Server responded with an error
+          if (e.response!.statusCode == 400) {
+            // Handle HTTP 400 Bad Request error
+            log('Bad Request - Server returned 400 status code');
+            throw Exception('Failed to postCancel');
+
+            // Additional error handling logic here if needed
+          } else {
+            // Handle other HTTP status codes
+            log('Server error - Status code: ${e.response!.statusCode}');
+            throw Exception('Failed to postCancel.');
+            // Additional error handling logic here if needed
+          }
+        } else {
+          // No response from the server (network error, timeout, etc.)
+          log('Dio error: ${e.message}');
+          throw Exception('Failed to postCancel.');
+        }
+      } else {
+        // Handle other exceptions if necessary
+        log('Error: $e');
+        throw Exception('Failed to postCancel.');
+      }
+    }
+  }
 }
