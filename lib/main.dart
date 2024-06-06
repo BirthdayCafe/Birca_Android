603f403e945147dadfccb6af46a68ca621baa0b0
cityshin <ssk7750@naver.com>
2024-06-06 19:55:15 +0900

    feature. main

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ import 'package:birca/viewmodel/select_interest_artist_viewmodel.dart';
 import 'package:birca/viewModel/business_license_view_model.dart';
 import 'package:birca/viewModel/nickname_view_model.dart';
 import 'package:birca/viewModel/visitor_search_result_view_model.dart';
-import 'package:birca/widgets/bottom_nav_owner.dart';
+import 'package:birca/widgets/bottom_nav_host.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:kakao_flutter_sdk/kakao_flutter_sdk.dart';
@@ -44,19 +44,12 @@ void main() async {
       ChangeNotifierProvider(create: (context) => BirthdayCafeViewModel()),
       ChangeNotifierProvider(create: (context) => HostMyCafeViewModel()),
       ChangeNotifierProvider(create: (context) => OwnerHomeViewModel()),
-      ChangeNotifierProvider(create: (context) => OwnerRequestDetailViewModel()),
+      ChangeNotifierProvider(
+          create: (context) => OwnerRequestDetailViewModel()),
       ChangeNotifierProvider(create: (context) => MypageViewModel()),
       ChangeNotifierProvider(create: (context) => OwnerMyCafeViewModel()),
       ChangeNotifierProvider(create: (context) => HostHomeViewModel()),
       ChangeNotifierProvider(create: (context) => OwnerScheduleViewModel()),
-
-
-
-
-
-
-
-
     ],
     child: const Birca(),
   ));
@@ -74,11 +67,11 @@ class Birca extends StatelessWidget {
         primaryColor: Palette.primary,
         fontFamily: "Pretendard",
         textTheme:
-        const TextTheme(bodyMedium: TextStyle(color: Color(0xff303031))),
+            const TextTheme(bodyMedium: TextStyle(color: Color(0xff303031))),
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.white),
         useMaterial3: true,
       ),
-      home: const BottomNavOwner(),
+      home: const BottomNavHost(),
     );
   }
 }
