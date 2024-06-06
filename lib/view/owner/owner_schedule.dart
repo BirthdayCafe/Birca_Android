45ecd4cb297a2a96c24f4b4dac2b52338ba6efca
cityshin <ssk7750@naver.com>
2024-06-06 16:07:22 +0900

    fix. api 수정 중

--- lib/view/owner/owner_schedule.dart
+++ lib/view/owner/owner_schedule.dart
@@ -14,8 +14,6 @@ class OwnerSchedule extends StatefulWidget {
 }
 
 class _OwnerSchedule extends State<OwnerSchedule> {
-
-
   //table calendar 변수
   bool isSwitched = false;
   String hostDate = '';
@@ -28,12 +26,8 @@ class _OwnerSchedule extends State<OwnerSchedule> {
   @override
   void initState() {
     super.initState();
-    Provider.of<OwnerScheduleViewModel>(context,
-        listen: false).getSchedule(DateTime
-        .now()
-        .year, DateTime
-        .now()
-        .month);
+    Provider.of<OwnerScheduleViewModel>(context, listen: false)
+        .getSchedule(DateTime.now().year, DateTime.now().month);
   }
 
   @override
@@ -50,105 +44,108 @@ class _OwnerSchedule extends State<OwnerSchedule> {
                   fontWeight: FontWeight.w700,
                   fontFamily: 'Pretendard',
                   color: Palette.primary),
-            ), IconButton(onPressed: () {
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => const OwnerScheduleAdd()));
-            }, icon: const Icon(Icons.add, size: 30,))
-
+            ),
+            IconButton(
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const OwnerScheduleAdd()));
+                },
+                icon: const Icon(
+                  Icons.add,
+                  size: 30,
+                ))
           ],
         ),
         centerTitle: false,
       ),
       body: SingleChildScrollView(
           child: Column(
-            children: [
-              Container(
-                padding: const EdgeInsets.only(
-                    left: 25, right: 25, top: 10, bottom: 13),
-                child: TableCalendar(
-                  //오늘 날짜
-                  focusedDay: _focusedDay,
-                  firstDay: DateTime.now(),
-                  lastDay: DateTime.utc(DateTime
-                      .now()
-                      .year + 1),
-
-                  headerStyle: const HeaderStyle(
-                      formatButtonVisible: false, titleCentered: true),
-
-
-                  selectedDayPredicate: (day) {
-                    return isSameDay(_selectedDay, day);
-                  },
-
-                  onDaySelected: (selectedDay, focusedDay) {
-                    if (!isSameDay(_selectedDay, selectedDay)) {
-                      setState(() {
-                        _selectedDay = selectedDay;
-                        _focusedDay =
-                            focusedDay; // update `_focusedDay` here as well
-                        log(_selectedDay.toString());
-                      });
-                    }
-                  },
-
-                  calendarStyle: const CalendarStyle(
-                    // isTodayHighlighted: false,
-                      selectedDecoration: BoxDecoration(
-                          color: Palette.primary,
-                          shape: BoxShape.circle
-                      )
-                  ),
-                  //달력 날짜 범위 선택
-                  onRangeSelected: (start, end, focusedDay) {
-                    setState(() {
-                      _selectedDay = null;
-                      _focusedDay = focusedDay;
-                    });
-                  },
+        children: [
+          Container(
+            padding:
+                const EdgeInsets.only(left: 25, right: 25, top: 10, bottom: 13),
+            child: TableCalendar(
+              //오늘 날짜
+              focusedDay: _focusedDay,
+              firstDay: DateTime.now(),
+              lastDay: DateTime.utc(DateTime.now().year + 1),
+              headerStyle: const HeaderStyle(
+                  formatButtonVisible: false, titleCentered: true),
+              selectedDayPredicate: (day) {
+                return isSameDay(_selectedDay, day);
+              },
+              onDaySelected: (selectedDay, focusedDay) {
+                if (!isSameDay(_selectedDay, selectedDay)) {
+                  setState(() {
+                    _selectedDay = selectedDay;
+                    _focusedDay =
+                        focusedDay; // update `_focusedDay` here as well
+                    log(_selectedDay.toString());
+                  });
+                }
+              },
+              calendarStyle: const CalendarStyle(
+                  // isTodayHighlighted: false,
+                  selectedDecoration: BoxDecoration(
+                      color: Palette.primary, shape: BoxShape.circle)),
+              //달력 날짜 범위 선택
+              onRangeSelected: (start, end, focusedDay) {
+                setState(() {
+                  _selectedDay = null;
+                  _focusedDay = focusedDay;
+                });
+              },
+            ),
+          ),
+          Container(
+            // height: 140,
+            width: double.infinity,
+            margin: const EdgeInsets.only(left: 14, right: 14, top: 19),
+            padding: const EdgeInsets.only(left: 26, top: 31, bottom: 28.5),
+            decoration: BoxDecoration(
+              color: Palette.gray02, // Container의 배경색
+              borderRadius: BorderRadius.circular(6),
+            ),
+            child: const Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "아티스트 : 뉴진스 민지",
+                  style: TextStyle(
+                      color: Palette.primary,
+                      fontFamily: 'Pretendard',
+                      fontWeight: FontWeight.w700,
+                      fontSize: 14),
                 ),
-              ),
-
-              Container(
-                // height: 140,
-                width: double.infinity,
-                margin: const EdgeInsets.only(left: 14, right: 14, top: 19),
-                padding: const EdgeInsets.only(left: 26, top: 31, bottom: 28.5),
-                decoration: BoxDecoration(
-                  color: Palette.gray02, // Container의 배경색
-                  borderRadius: BorderRadius.circular(6),
+                SizedBox(
+                  height: 13.5,
                 ),
-                child: const Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text("아티스트 : 뉴진스 민지", style: TextStyle(
-                        color: Palette.primary,
-                        fontFamily: 'Pretendard',
-                        fontWeight: FontWeight.w700,
-                        fontSize: 14),),
-                    SizedBox(height: 13.5,),
-                    Text("닉네임 : 홍길동", style: TextStyle(color: Palette.gray10,
-                        fontFamily: 'Pretendard',
-                        fontWeight: FontWeight.w500,
-                        fontSize: 14),),
-                    SizedBox(height: 13.5,),
-                    Text("일정 : 2024.01.01~2024.01.01", style: TextStyle(
-                        color: Palette.gray10,
-                        fontFamily: 'Pretendard',
-                        fontWeight: FontWeight.w500,
-                        fontSize: 14),)
-
-                  ],
-
+                Text(
+                  "닉네임 : 홍길동",
+                  style: TextStyle(
+                      color: Palette.gray10,
+                      fontFamily: 'Pretendard',
+                      fontWeight: FontWeight.w500,
+                      fontSize: 14),
                 ),
-              )
-            ],
+                SizedBox(
+                  height: 13.5,
+                ),
+                Text(
+                  "일정 : 2024.01.01~2024.01.01",
+                  style: TextStyle(
+                      color: Palette.gray10,
+                      fontFamily: 'Pretendard',
+                      fontWeight: FontWeight.w500,
+                      fontSize: 14),
+                )
+              ],
+            ),
           )
-      ),
-
+        ],
+      )),
     );
   }
-
 }
