4f2e9762a2dc6779c2ba503565c9838462aa410f
cityshin <ssk7750@naver.com>
2024-06-07 15:28:49 +0900

    feature. 주최자 생일 카페 대표 정보 수정 model

--- lib/model/birthday_cafe_model.dart
+++ lib/model/birthday_cafe_model.dart
@@ -1,40 +1,38 @@
-class BirthdayCafeModel{
+class BirthdayCafeModel {
+  Cafe cafe;
+  Artist artist;
+  String startDate;
+  String endDate;
+  String birthdayCafeName;
+  int likeCount;
+  bool isLiked;
+  String twitterAccount;
+  int minimumVisitant;
+  int maximumVisitant;
+  String congestionState;
+  String visibility;
+  String progressState;
+  String specialGoodsStockState;
+  String mainImage;
+  List<String> defaultImages;
 
-   Cafe cafe;
-   Artist artist;
-   String startDate;
-   String endDate;
-   String birthdayCafeName;
-   int likeCount;
-   bool isLiked;
-   String twitterAccount;
-   int minimumVisitant;
-   int maximumVisitant;
-   String congestionState;
-   String visibility;
-   String progressState;
-   String specialGoodsStockState;
-   String mainImage;
-   List<String> defaultImages;
-
-  BirthdayCafeModel({
-    required this.cafe,
-    required this.artist,
-    required this.startDate,
-    required this.endDate,
-    required this.birthdayCafeName,
-    required this.likeCount,
-    required this.isLiked,
-    required this.twitterAccount,
-    required this.minimumVisitant,
-    required this.maximumVisitant,
-    required this.congestionState,
-    required this.visibility,
-    required this.progressState,
-    required this.specialGoodsStockState,
-    required this.mainImage,
-    required this.defaultImages
-});
+  BirthdayCafeModel(
+      {required this.cafe,
+      required this.artist,
+      required this.startDate,
+      required this.endDate,
+      required this.birthdayCafeName,
+      required this.likeCount,
+      required this.isLiked,
+      required this.twitterAccount,
+      required this.minimumVisitant,
+      required this.maximumVisitant,
+      required this.congestionState,
+      required this.visibility,
+      required this.progressState,
+      required this.specialGoodsStockState,
+      required this.mainImage,
+      required this.defaultImages});
 
   factory BirthdayCafeModel.fromJson(Map<String, dynamic> json) {
     return BirthdayCafeModel(
@@ -56,12 +54,11 @@ class BirthdayCafeModel{
       defaultImages: List<String>.from(json['defaultImages']),
     );
   }
-
 }
 
 class Artist {
-   String groupName;
-   String name;
+  String groupName;
+  String name;
 
   Artist({required this.groupName, required this.name});
 
@@ -74,9 +71,9 @@ class Artist {
 }
 
 class Cafe {
-   String address;
-   String name;
-   List<String> images;
+  String address;
+  String name;
+  List<String> images;
 
   Cafe({required this.name, required this.address, required this.images});
 
@@ -86,5 +83,29 @@ class Cafe {
       address: json['address'],
       images: List<String>.from(json['images']),
     );
+  }
+}
+
+class BirthdayCafeInfoModel {
+  String? birthdayCafeName;
+  String? birthdayCafeTwitterAccount;
+
+  BirthdayCafeInfoModel(
+      {this.birthdayCafeName, this.birthdayCafeTwitterAccount});
+
+  // JSON 데이터를 모델 객체로 변환하는 factory 생성자
+  factory BirthdayCafeInfoModel.fromJson(Map<String, dynamic> json) {
+    return BirthdayCafeInfoModel(
+      birthdayCafeName: json['birthdayCafeName'],
+      birthdayCafeTwitterAccount: json['birthdayCafeTwitterAccount'],
+    );
+  }
+
+  // 모델 객체를 JSON 데이터로 변환하는 메소드
+  Map<String, dynamic> toJson() {
+    return {
+      'birthdayCafeName': birthdayCafeName,
+      'birthdayCafeTwitterAccount': birthdayCafeTwitterAccount,
+    };
   }
 }
