6c81faffc14b8549e326097e10dd46ae73a497f9
cityshin <ssk7750@naver.com>
2024-06-07 14:18:25 +0900

    feature. 주최자 생일 카페 이미지 업로드 api

--- lib/viewModel/birthday_cafe_view_model.dart
+++ lib/viewModel/birthday_cafe_view_model.dart
@@ -1,4 +1,5 @@
 import 'dart:developer';
+import 'dart:io';
 import 'package:birca/model/birthday_cafe_lucky_draws_model.dart';
 import 'package:birca/model/birthday_cafe_menus_model.dart';
 import 'package:birca/model/birthday_cafe_model.dart';
@@ -6,6 +7,7 @@ import 'package:birca/model/birthday_cafe_special_goods_model.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:image_picker/image_picker.dart';
 
 class BirthdayCafeViewModel extends ChangeNotifier {
   Dio dio = Dio();
@@ -345,4 +347,80 @@ class BirthdayCafeViewModel extends ChangeNotifier {
       }
     }
   }
+
+  //생일 카페 사진 편집
+  Future<void> postImage(int cafeId, List<PickedFile> pickedFiles) async {
+    // const storage = FlutterSecureStorage();
+    var baseUrl = dotenv.env['BASE_URL'];
+    var token = '';
+
+        token ='eyJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwiaWF0IjoxNzEyMjMxMzYwLCJleHAiOjE3MzAyMzEzNjB9.Rz0qqN10T-ZM2L0PC1hFd_UR5X9djywjhyiINTTd3M4';
+    // var kakaoLoginInfo = await storage.read(key: 'kakaoLoginInfo');
+    //
+    // // 토큰 가져오기
+    // if (kakaoLoginInfo != null) {
+    //   Map<String, dynamic> loginData = json.decode(kakaoLoginInfo);
+    //   token = loginData['accessToken'].toString();
+    // }
+
+
+    // PickedFile 리스트를 File 리스트로 변환
+    List<File> cafeImages =
+    pickedFiles.map((pickedFile) => File(pickedFile.path)).toList();
+
+    // 업로드할 파일을 FormData로 변환
+    FormData formData = FormData();
+    for (int i = 0; i < cafeImages.length; i++) {
+      formData.files
+          .add(MapEntry('defaultImage', await MultipartFile.fromFile(cafeImages[i].path,)));
+    }
+    // LogInterceptor 추가
+    dio.interceptors.add(LogInterceptor(
+      requestBody: true,
+      responseBody: true,
+    ));
+
+    log(formData.toString());
+
+    try {
+      // API 엔드포인트 및 업로드
+      Response response = await dio.post(
+        '${baseUrl}api/v1/birthday-cafes/$cafeId/images',
+        data: formData,
+        options: Options(headers: {'Authorization': 'Bearer $token'}, ),);
+
+      // 서버 응답 출력
+      log('postImage Response: ${response.data}');
+
+      notifyListeners();
+    } catch (e) {
+      if (e is DioException) {
+        // Dio exception handling
+        if (e.response != null) {
+          // Server responded with an error
+          if (e.response!.statusCode == 400) {
+            // Handle HTTP 400 Bad Request error
+            log('Bad Request - Server returned 400 status code');
+            throw Exception('Failed to postImage');
+
+            // Additional error handling logic here if needed
+          } else {
+            // Handle other HTTP status codes
+            log('Server error - Status code: ${e.response!.statusCode}');
+            throw Exception('Failed to postImage.');
+            // Additional error handling logic here if needed
+          }
+        } else {
+          // No response from the server (network error, timeout, etc.)
+          log('Dio error: ${e.message}');
+          throw Exception('Failed to postImage.');
+        }
+      } else {
+        // Handle other exceptions if necessary
+        log('Error: $e');
+        throw Exception('Failed to postImage.');
+      }
+    }
+  }
+
 }
