b1908ea258c73e6d119b602619161f0f5e9e93d8
cityshin <ssk7750@naver.com>
2024-06-13 16:47:53 +0900

    feature. 예약된 날짜 가져오기 api

--- lib/viewModel/owner_schedule_view_model.dart
+++ lib/viewModel/owner_schedule_view_model.dart
@@ -12,6 +12,9 @@ class OwnerScheduleViewModel extends ChangeNotifier {
 
   OwnerScheduleModel? get ownerScheduleModel => _ownerScheduleModel;
 
+  List<OwnerScheduleExistModel>? _ownerScheduleExistModel;
+  List<OwnerScheduleExistModel>? get ownerScheduleExistModel => _ownerScheduleExistModel;
+
   //사장님 스케줄 추가
   Future<void> postSchedule(OwnerScheduleAddModel ownerScheduleAddModel) async {
     // const storage = FlutterSecureStorage();
@@ -76,7 +79,7 @@ class OwnerScheduleViewModel extends ChangeNotifier {
   }
 
   //사장님 스케줄 가져오기
-  Future<void> getSchedule(String dateTime) async {
+  Future<void> getScheduleDetail(String dateTime) async {
     // const storage = FlutterSecureStorage();
     var baseUrl = dotenv.env['BASE_URL'];
     var token = '';
@@ -100,18 +103,90 @@ class OwnerScheduleViewModel extends ChangeNotifier {
     try {
       // API 엔드포인트 및 업로드
       Response response = await dio.get(
-          '${baseUrl}api/v1/owners/birthday-cafes/schedules',
+          '${baseUrl}api/v1/owners/birthday-cafes/schedules/detail',
           queryParameters: {'date': dateTime},
           options: Options(headers: {'Authorization': 'Bearer $token'}));
 
       _ownerScheduleModel = null;
 
       // 서버 응답 출력
-      log('getSchedule Response: ${response.data}');
+      log('getScheduleDetail Response: ${response.data}');
 
       if (response.data != null && response.data.isNotEmpty) {
         _ownerScheduleModel = OwnerScheduleModel.fromJson(response.data);
       }
+
+      notifyListeners();
+    } catch (e) {
+      if (e is DioException) {
+        // Dio exception handling
+        if (e.response != null) {
+          // Server responded with an error
+          if (e.response!.statusCode == 400) {
+            // Handle HTTP 400 Bad Request error
+            log('Bad Request - Server returned 400 status code');
+            throw Exception('Failed to getScheduleDetail');
+
+            // Additional error handling logic here if needed
+          } else {
+            // Handle other HTTP status codes
+            log('Server error - Status code: ${e.response!.statusCode}');
+            throw Exception('Failed to getScheduleDetail.');
+            // Additional error handling logic here if needed
+          }
+        } else {
+          // No response from the server (network error, timeout, etc.)
+          log('Dio error: ${e.message}');
+          throw Exception('Failed to getScheduleDetail.');
+        }
+      } else {
+        // Handle other exceptions if necessary
+        log('Error: $e');
+        throw Exception('Failed to getScheduleDetail.');
+      }
+    }
+  }
+
+  //사장님 예약 날짜 가져오기
+  Future<void> getSchedule(int year, int month) async {
+    // const storage = FlutterSecureStorage();
+    var baseUrl = dotenv.env['BASE_URL'];
+    var token = '';
+
+    token =
+    "eyJhbGciOiJIUzI1NiJ9.eyJpZCI6MywiaWF0IjoxNzE1NjQ4MTk1LCJleHAiOjE3MjA4MzIxOTV9.yFY9Y18aPo4t1XA5ANsnfvqqnJsmq7kalNfj7FcGEi4";
+    // var kakaoLoginInfo = await storage.read(key: 'kakaoLoginInfo');
+    //
+    // // 토큰 가져오기
+    // if (kakaoLoginInfo != null) {
+    //   Map<String, dynamic> loginData = json.decode(kakaoLoginInfo);
+    //   token = loginData['accessToken'].toString();
+    // }
+
+    // LogInterceptor 추가
+    dio.interceptors.add(LogInterceptor(
+      requestBody: true,
+      responseBody: true,
+    ));
+
+    try {
+      // API 엔드포인트 및 업로드
+      Response response = await dio.get(
+          '${baseUrl}api/v1/owners/birthday-cafes/schedules',
+          queryParameters: {'year': year,'month':month},
+          options: Options(headers: {'Authorization': 'Bearer $token'}));
+
+      _ownerScheduleExistModel = [];
+
+      List<dynamic> jsonData = response.data;
+      List<OwnerScheduleExistModel> cafeHomeModels =
+      jsonData.map((e) => OwnerScheduleExistModel.fromJson(e)).toList();
+
+      // _hostCafeHomeModelList 추가
+      _ownerScheduleExistModel?.addAll(cafeHomeModels);
+      // 서버 응답 출력
+      log('getSchedule Response: ${response.data}');
+
 
       notifyListeners();
     } catch (e) {
