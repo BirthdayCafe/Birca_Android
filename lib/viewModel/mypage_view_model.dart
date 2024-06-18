c8b1f72f3823cd42c90a86a1f48cb99360a1e357
cityshin <ssk7750@naver.com>
2024-06-18 15:23:12 +0900

    feature. 역할 변경 api

--- lib/viewModel/mypage_view_model.dart
+++ lib/viewModel/mypage_view_model.dart
@@ -7,7 +7,6 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import '../model/my_page_model.dart';
 
 class MypageViewModel extends ChangeNotifier {
-
   MypageModel? _nickname;
 
   MypageModel? get nickname => _nickname;
@@ -20,7 +19,6 @@ class MypageViewModel extends ChangeNotifier {
 
   //닉네임 가져오기
   Future<void> getNickName() async {
-
     var kakaoLoginInfo = await storage.read(key: 'kakaoLoginInfo');
 
     // 토큰 가져오기
@@ -71,4 +69,34 @@ class MypageViewModel extends ChangeNotifier {
     }
   }
 
+  //post role change
+  Future<void> postRoleChange(String role) async {
+    const storage = FlutterSecureStorage();
+
+    Dio dio = Dio();
+    Response response;
+    var baseUrl = dotenv.env['BASE_URL'];
+
+    var token = '';
+    var kakaoLoginInfo = await storage.read(key: 'kakaoLoginInfo');
+
+    //토큰 가져오기
+    if (kakaoLoginInfo != null) {
+      Map<String, dynamic> loginData = json.decode(kakaoLoginInfo);
+      token = loginData['accessToken'].toString();
+    }
+
+    try {
+      response = await dio.post('${baseUrl}api/v1/members/role-change',
+          data: {'role': role},
+          options: Options(headers: {'Authorization': 'Bearer $token'}));
+      // log('kakaoLoginInfo : $kakaoLoginInfo');
+
+      log('$role 변경 ');
+
+      log(response.data.toString());
+    } catch (e) {
+      log(e.toString());
+    }
+  }
 }
