988d17581b546eb943c0d68860b456c1825a1d50
cityshin <ssk7750@naver.com>
2024-06-20 16:38:00 +0900

    fix. logInterceptor 및 통신 error 함수 분리

--- lib/viewModel/owner_home_view_model.dart
+++ lib/viewModel/owner_home_view_model.dart
@@ -4,10 +4,12 @@ import 'package:dio/dio.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
+import '../model/api.dart';
 import '../model/owner_home_model.dart';
 
 class OwnerHomeViewModel extends ChangeNotifier{
   Dio dio = Dio();
+  Api api = Api();
 
   List<OwnerHomeModel>? _ownerHomeModelList;
   List<OwnerHomeModel>? get ownerHomeModelList =>
@@ -32,11 +34,7 @@ class OwnerHomeViewModel extends ChangeNotifier{
     // }
     token ="eyJhbGciOiJIUzI1NiJ9.eyJpZCI6MywiaWF0IjoxNzE1NjQ4MTk1LCJleHAiOjE3MjA4MzIxOTV9.yFY9Y18aPo4t1XA5ANsnfvqqnJsmq7kalNfj7FcGEi4";
 
-    // LogInterceptor 추가
-    dio.interceptors.add(LogInterceptor(
-      requestBody: true,
-      responseBody: true,
-    ));
+    api.logInterceptor();
 
     _ownerHomeModelList = [];
 
@@ -61,32 +59,7 @@ class OwnerHomeViewModel extends ChangeNotifier{
 
       notifyListeners();
     } catch (e) {
-      if (e is DioException) {
-        // Dio exception handling
-        if (e.response != null) {
-          // Server responded with an error
-          if (e.response!.statusCode == 400) {
-            // Handle HTTP 400 Bad Request error
-            log('Bad Request - Server returned 400 status code');
-            throw Exception('Failed to getOwnerHome');
-
-            // Additional error handling logic here if needed
-          } else {
-            // Handle other HTTP status codes
-            log('Server error - Status code: ${e.response!.statusCode}');
-            throw Exception('Failed to getOwnerHome.');
-            // Additional error handling logic here if needed
-          }
-        } else {
-          // No response from the server (network error, timeout, etc.)
-          log('Dio error: ${e.message}');
-          throw Exception('Failed to getOwnerHome.');
-        }
-      } else {
-        // Handle other exceptions if necessary
-        log('Error: $e');
-        throw Exception('Failed to getOwnerHome.');
-      }
+      api.errorCheck(e);
     }
   }
 }
