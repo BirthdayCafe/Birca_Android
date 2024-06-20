14db40113baf9160e1f3a8f915460af643b60391
cityshin <ssk7750@naver.com>
2024-06-20 16:02:48 +0900

    fix. logInterceptor 와 통신 error 함수 분리

--- lib/viewModel/host_my_cafe_view_model.dart
+++ lib/viewModel/host_my_cafe_view_model.dart
@@ -6,15 +6,14 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import '../model/host_my_cafe_model.dart';
 
-class HostMyCafeViewModel extends ChangeNotifier{
+class HostMyCafeViewModel extends ChangeNotifier {
   Dio dio = Dio();
 
   List<HostMyCafeModel>? _hostMyCafeModelList;
 
-  List<HostMyCafeModel>? get hostMyCafeModelList =>
-      _hostMyCafeModelList;
+  List<HostMyCafeModel>? get hostMyCafeModelList => _hostMyCafeModelList;
 
-  HostMyCafeViewModel(){
+  HostMyCafeViewModel() {
     _hostMyCafeModelList = [];
   }
 
@@ -38,8 +37,6 @@ class HostMyCafeViewModel extends ChangeNotifier{
 
   //주최자 나의 생일 카페 목록 가져오기
   Future<void> getHostMyCafe() async {
-
-
     var kakaoLoginInfo = await storage.read(key: 'kakaoLoginInfo');
 
     // 토큰 가져오기
@@ -48,16 +45,11 @@ class HostMyCafeViewModel extends ChangeNotifier{
       token = loginData['accessToken'].toString();
     }
 
-    // LogInterceptor 추가
-    dio.interceptors.add(LogInterceptor(
-      requestBody: true,
-      responseBody: true,
-    ));
+    logInterceptor();
 
     try {
       // API 엔드포인트 및 업로드
       Response response = await dio.get('${baseUrl}api/v1/birthday-cafes/me',
-
           options: Options(headers: {'Authorization': 'Bearer $token'}));
 
       // 서버 응답 출력
@@ -65,8 +57,7 @@ class HostMyCafeViewModel extends ChangeNotifier{
       _hostMyCafeModelList = [];
       List<dynamic> jsonData = response.data;
       List<HostMyCafeModel> cafeHomeModels =
-      jsonData.map((e) => HostMyCafeModel.fromJson(e)).toList();
-
+          jsonData.map((e) => HostMyCafeModel.fromJson(e)).toList();
 
       // _hostCafeHomeModelList 추가
       _hostMyCafeModelList?.addAll(cafeHomeModels);
@@ -74,39 +65,12 @@ class HostMyCafeViewModel extends ChangeNotifier{
       log("model  $_hostMyCafeModelList");
       notifyListeners();
     } catch (e) {
-      if (e is DioException) {
-        // Dio exception handling
-        if (e.response != null) {
-          // Server responded with an error
-          if (e.response!.statusCode == 400) {
-            // Handle HTTP 400 Bad Request error
-            log('Bad Request - Server returned 400 status code');
-            throw Exception('Failed to getHostMyCafe');
-
-            // Additional error handling logic here if needed
-          } else {
-            // Handle other HTTP status codes
-            log('Server error - Status code: ${e.response!.statusCode}');
-            throw Exception('Failed to getHostMyCafe.');
-            // Additional error handling logic here if needed
-          }
-        } else {
-          // No response from the server (network error, timeout, etc.)
-          log('Dio error: ${e.message}');
-          throw Exception('Failed to getHostMyCafe.');
-        }
-      } else {
-        // Handle other exceptions if necessary
-        log('Error: $e');
-        throw Exception('Failed to getHostMyCafe.');
-      }
+      errorCheck(e);
     }
   }
 
   //주최자 나의 생일 카페 대관 신청 취소 가져오기
   Future<void> postCancel(int cafeId) async {
-
-
     var kakaoLoginInfo = await storage.read(key: 'kakaoLoginInfo');
 
     // 토큰 가져오기
@@ -115,16 +79,12 @@ class HostMyCafeViewModel extends ChangeNotifier{
       token = loginData['accessToken'].toString();
     }
 
-    // LogInterceptor 추가
-    dio.interceptors.add(LogInterceptor(
-      requestBody: true,
-      responseBody: true,
-    ));
+    logInterceptor();
 
     try {
       // API 엔드포인트 및 업로드
-      Response response = await dio.post('${baseUrl}api/v1/birthday-cafes/$cafeId/cancel',
-
+      Response response = await dio.post(
+          '${baseUrl}api/v1/birthday-cafes/$cafeId/cancel',
           options: Options(headers: {'Authorization': 'Bearer $token'}));
 
       // 서버 응답 출력
@@ -132,32 +92,45 @@ class HostMyCafeViewModel extends ChangeNotifier{
 
       notifyListeners();
     } catch (e) {
-      if (e is DioException) {
-        // Dio exception handling
-        if (e.response != null) {
-          // Server responded with an error
-          if (e.response!.statusCode == 400) {
-            // Handle HTTP 400 Bad Request error
-            log('Bad Request - Server returned 400 status code');
-            throw Exception('Failed to postCancel');
+      errorCheck(e);
+    }
+  }
 
-            // Additional error handling logic here if needed
-          } else {
-            // Handle other HTTP status codes
-            log('Server error - Status code: ${e.response!.statusCode}');
-            throw Exception('Failed to postCancel.');
-            // Additional error handling logic here if needed
-          }
+  //통신 error 검사
+  void errorCheck(e) {
+    if (e is DioException) {
+      // Dio exception handling
+      if (e.response != null) {
+        // Server responded with an error
+        if (e.response!.statusCode == 400) {
+          // Handle HTTP 400 Bad Request error
+          log('Bad Request - Server returned 400 status code');
+          throw Exception('Failed 1');
+
+          // Additional error handling logic here if needed
         } else {
-          // No response from the server (network error, timeout, etc.)
-          log('Dio error: ${e.message}');
-          throw Exception('Failed to postCancel.');
+          // Handle other HTTP status codes
+          log('Server error - Status code: ${e.response!.statusCode}');
+          throw Exception('Failed 2');
+          // Additional error handling logic here if needed
         }
       } else {
-        // Handle other exceptions if necessary
-        log('Error: $e');
-        throw Exception('Failed to postCancel.');
+        // No response from the server (network error, timeout, etc.)
+        log('Dio error: ${e.message}');
+        throw Exception('Failed 3');
       }
+    } else {
+      // Handle other exceptions if necessary
+      log('Error: $e');
+      throw Exception('Failed 4');
     }
+  }
+
+  //LogInterceptor 추가
+  void logInterceptor() {
+    dio.interceptors.add(LogInterceptor(
+      requestBody: true,
+      responseBody: true,
+    ));
   }
 }
