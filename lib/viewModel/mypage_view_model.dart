c4b8fa27134ac16246809a65c7af48f06bad1622
cityshin <ssk7750@naver.com>
2024-06-20 16:05:13 +0900

    fix. logInterceptor 와 통신 error 함수 분리

--- lib/viewModel/mypage_view_model.dart
+++ lib/viewModel/mypage_view_model.dart
@@ -26,11 +26,7 @@ class MypageViewModel extends ChangeNotifier {
       Map<String, dynamic> loginData = json.decode(kakaoLoginInfo);
       token = loginData['accessToken'].toString();
     }
-    // LogInterceptor 추가
-    dio.interceptors.add(LogInterceptor(
-      requestBody: true,
-      responseBody: true,
-    ));
+    logInterceptor();
 
     try {
       // API 엔드포인트 및 업로드
@@ -43,29 +39,7 @@ class MypageViewModel extends ChangeNotifier {
 
       notifyListeners();
     } catch (e) {
-      if (e is DioException) {
-        // Dio exception handling
-        if (e.response != null) {
-          // Server responded with an error
-          if (e.response!.statusCode == 400) {
-            // Handle HTTP 400 Bad Request error
-            log('Bad Request - Server returned 400 status code');
-            log('Response data: ${e.response!.data}');
-            // Additional error handling logic here if needed
-          } else {
-            // Handle other HTTP status codes
-            log('Server error - Status code: ${e.response!.statusCode}');
-            log('Response data: ${e.response!.data}');
-            // Additional error handling logic here if needed
-          }
-        } else {
-          // No response from the server (network error, timeout, etc.)
-          log('Dio error: ${e.message}');
-        }
-      } else {
-        // Handle other exceptions if necessary
-        log('Error: $e');
-      }
+      errorCheck(e);
     }
   }
 
@@ -79,6 +53,8 @@ class MypageViewModel extends ChangeNotifier {
 
     var token = '';
     var kakaoLoginInfo = await storage.read(key: 'kakaoLoginInfo');
+
+    logInterceptor();
 
     //토큰 가져오기
     if (kakaoLoginInfo != null) {
@@ -96,7 +72,45 @@ class MypageViewModel extends ChangeNotifier {
 
       log(response.data.toString());
     } catch (e) {
-      log(e.toString());
+      errorCheck(e);
     }
+  }
+
+  //통신 error 검사
+  void errorCheck(e) {
+    if (e is DioException) {
+      // Dio exception handling
+      if (e.response != null) {
+        // Server responded with an error
+        if (e.response!.statusCode == 400) {
+          // Handle HTTP 400 Bad Request error
+          log('Bad Request - Server returned 400 status code');
+          throw Exception('Failed 1');
+
+          // Additional error handling logic here if needed
+        } else {
+          // Handle other HTTP status codes
+          log('Server error - Status code: ${e.response!.statusCode}');
+          throw Exception('Failed 2');
+          // Additional error handling logic here if needed
+        }
+      } else {
+        // No response from the server (network error, timeout, etc.)
+        log('Dio error: ${e.message}');
+        throw Exception('Failed 3');
+      }
+    } else {
+      // Handle other exceptions if necessary
+      log('Error: $e');
+      throw Exception('Failed 4');
+    }
+  }
+
+  //LogInterceptor 추가
+  void logInterceptor() {
+    dio.interceptors.add(LogInterceptor(
+      requestBody: true,
+      responseBody: true,
+    ));
   }
 }
