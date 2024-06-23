8fa202de171ef742dc4a76c56660999cbfbbeec0
cityshin <ssk7750@naver.com>
2024-06-23 18:46:58 +0900

    fix. A RenderFlex overflowed by 7.5 pixels on the bottom.

--- lib/widgets/item.dart
+++ lib/widgets/item.dart
@@ -1,26 +1,28 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-
 import '../designSystem/palette.dart';
 
 artistItem(String path, String artistName) => Column(
+      mainAxisSize: MainAxisSize.min,
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
         SizedBox(
-          height: 70,
-          width: 70,
+          height: 60,
+          width: 60,
           child: ClipRRect(
             borderRadius: BorderRadius.circular(50),
             child: Image.network(
               path,
               fit: BoxFit.cover,
-              errorBuilder: (BuildContext context, Object exception, StackTrace? stackTrace) {
+              errorBuilder: (BuildContext context, Object exception,
+                  StackTrace? stackTrace) {
                 return Image.asset('lib/assets/image/artist.svg');
               },
             ),
           ),
         ),
-        const SizedBox(height: 6,),
+        const SizedBox(
+          height: 6,
+        ),
         Text(
           artistName,
           style: const TextStyle(color: Palette.gray06),
