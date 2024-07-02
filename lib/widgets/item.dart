8c9b9721e031914c6b8144c81b987d78e40b14af
cityshin <ssk7750@naver.com>
2024-07-03 01:00:31 +0900

    feature. 아티스트 선택 삭제 아이콘 추가

--- lib/widgets/item.dart
+++ lib/widgets/item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import '../designSystem/palette.dart';
 
 artistItem(String path, String artistName) => Column(
@@ -19,6 +20,48 @@ artistItem(String path, String artistName) => Column(
               },
             ),
           ),
+        ),
+        const SizedBox(
+          height: 6,
+        ),
+        Text(
+          artistName,
+          style: const TextStyle(color: Palette.gray06),
+        ),
+      ],
+    );
+
+artistSelectedItem(String path, String artistName) => Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Stack(
+          children: [
+            SizedBox(
+              height: 70,
+              width: 70,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(50),
+                child: Image.network(
+                  path,
+                  fit: BoxFit.cover,
+                  errorBuilder: (BuildContext context, Object exception,
+                      StackTrace? stackTrace) {
+                    return Image.asset('lib/assets/image/artist.svg');
+                  },
+                ),
+              ),
+            ),
+            Positioned(
+              top: 1,
+              right: 1,
+              child: SvgPicture.asset(
+                'lib/assets/image/unselect_artist.svg',
+                width: 24,
+                height: 24,
+              ),
+            )
+          ],
         ),
         const SizedBox(
           height: 6,
