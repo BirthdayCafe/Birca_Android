e1ae8574abb3855178030340303e201df8e2430b
cityshin <ssk7750@naver.com>
2024-08-31 15:30:10 +0900

    feature. WebView를 통한 약관 연결

--- lib/widgets/privacy_policy.dart
+++ lib/widgets/privacy_policy.dart
@@ -1,21 +1,55 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
-class PrivacyPolicy extends StatelessWidget {
+class PrivacyPolicy extends StatefulWidget {
   const PrivacyPolicy({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _PrivacyPolicy();
+}
+
+class _PrivacyPolicy extends State<PrivacyPolicy> {
+  late WebViewController controller;
+
+  @override
+  void initState() {
+    super.initState();
+
+    controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onProgress: (int progress) {
+            // Update loading bar.
+          },
+          onPageStarted: (String url) {},
+          onPageFinished: (String url) {},
+          onHttpError: (HttpResponseError error) {},
+          onWebResourceError: (WebResourceError error) {},
+          onNavigationRequest: (NavigationRequest request) {
+            if (request.url.startsWith('https://www.youtube.com/')) {
+              return NavigationDecision.prevent;
+            }
+            return NavigationDecision.navigate;
+          },
+        ),
+      )
+      ..loadRequest(
+          Uri.parse('http://54.180.145.199:8080/privacy_policy.html'));
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          leading: IconButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              icon: SvgPicture.asset('lib/assets/image/ic_back.svg')),
-        ),
-        body: const SingleChildScrollView(
-          child: Text('개인 정보 처리 방침'),
-        ));
+      appBar: AppBar(
+        leading: IconButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            icon: SvgPicture.asset('lib/assets/image/ic_back.svg')),
+      ),
+      body: WebViewWidget(controller: controller),
+    );
   }
 }
--- lib/widgets/service_policy.dart
+++ lib/widgets/service_policy.dart
@@ -1,21 +1,53 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
-class ServicePolicy extends StatelessWidget {
+class ServicePolicy extends StatefulWidget {
   const ServicePolicy({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _ServicePolicy();
+}
+
+class _ServicePolicy extends State<ServicePolicy> {
+  late WebViewController controller;
+
+  void initState() {
+    super.initState();
+
+    controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onProgress: (int progress) {
+            // Update loading bar.
+          },
+          onPageStarted: (String url) {},
+          onPageFinished: (String url) {},
+          onHttpError: (HttpResponseError error) {},
+          onWebResourceError: (WebResourceError error) {},
+          onNavigationRequest: (NavigationRequest request) {
+            if (request.url.startsWith('https://www.youtube.com/')) {
+              return NavigationDecision.prevent;
+            }
+            return NavigationDecision.navigate;
+          },
+        ),
+      )
+      ..loadRequest(Uri.parse('http://54.180.145.199:8080/service_terms.html'));
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          leading: IconButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              icon: SvgPicture.asset('lib/assets/image/ic_back.svg')),
-        ),
-        body: const SingleChildScrollView(
-          child: Text('서비스 이용 약관'),
-        ));
+      appBar: AppBar(
+        leading: IconButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            icon: SvgPicture.asset('lib/assets/image/ic_back.svg')),
+      ),
+      body: WebViewWidget(controller: controller),
+    );
   }
 }
